98676a35f112ad83ed27235b51ca1bf384f1b1fe
Johannes Hauschild <git@johannes-hauschild.de>
2016-12-19 19:10:54 +0100

    doc of tools/params

--- tenpy/tools/params.py
+++ tenpy/tools/params.py
@@ -1,26 +1,60 @@
 """Tools to handle paramters for algorithms.
 
-"""
+See the doc-string of :func:`get_parameter` for details."""
 
+__all__ = ["get_parameter"]
 
 def get_parameter(par_dict, key, default, descr=''):
-    """read out a parameter from the dictionary or provide default value.
+    """Read out a parameter from the dictionary and/or provide default values.
+
+    This function provides a similar functionality as ``par_dict.get(key, default)``.
+    *Unlike* `dict.get` this function writes the default value into the dictionary
+    (i.e. in other words it's more similar to ``par_dict.set_default(key, default)``).
+
+    However, a special key ``'verbose'`` (with value > 0) *in* the `par_dict`
+    triggers this function to additionally print the used (default) value.
+
+    This function should be used in the algorithms to read out parameters.
+    Then, when the algorithms are calleed by TenPy users,
+    simply including ``verbose=1`` into a parameter dictionary will trigger the algorithms to
+    print all the actually used parameters during runtime.
+
+    In addition, the user can save the modified dictionary along with other data, which gives a
+    concrete record of the actually used parameters and simplifies reproducing the results.
+
+    Parameters
+    ----------
+    par_dict : dict
+        A dicionary of the parameters as provided by the user.
+        If `key` is not a valid key, par_dict[key] is set to the `default`
+    key : string
+        The key for the parameter which should be read out from the dictionary.
+    default :
+        The default value for the parameter.
+    descr : str
+        A short description like 'TEBD', 'XXZ_model', which is used for verbose output.
+
+    Returns
+    -------
+    value :
+        ``par_dict[key]`` if the key is in par_dict, otherwise `default`.
 
     Examples
     --------
-    In a function of the algorithms, use this function for read-out of parameters:
+    :func:`~tenpy.algorithms.tebd.time_evolution` gets a dictionary of parameters.
+    Beside doing other stuff, it calls :meth:`tenpy.models.model.NearestNeighborModel.calc_U_bond`
+    with the dictionary as argument, which looks similar like:
 
     >>> def model_calc_U(U_param):
     >>>    dt = get_parameter(U_param, 'dt', 0.01, 'TEBD')
-    >>>    # calculate exp(-i * dt* H) ....
+    >>>    # ... calculate exp(-i * dt* H) ....
 
-    Then, when you call the algoithm without any parameters, they just use the default values:
+    Then, when you call `time_evolution` without any parameters, it just uses the default value:
 
-    >>> tenpy.algorithms.tebd.time_evolution(..., dict())  # calls model_calc_U at some point
+    >>> tenpy.algorithms.tebd.time_evolution(..., dict())  # uses dt=0.01
 
-    If you provide a special keyword ``'verbose'`` which will triger this function to print the
-    used parameter values.
-    This is an easy way to get a list of all the possible parameters you can change.
+    If you provide the special keyword ``'verbose'`` you can triger this function to print the
+    used parameter values:
 
     >>> tenpy.algorithms.tebd.time_evolution(..., dict(verbose=1))
     set parameter 'dt'=0.01 (default) for TEBD
