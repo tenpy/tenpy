cb495e25b032ede8b77952c17c5c912f39fcec5d
Johannes Hauschild <git@johannes-hauschild.de>
2017-07-31 12:25:36 +0200

    enhanced model test: check hermiticity of H

--- tests/test_mpo.py
+++ tests/test_mpo.py
@@ -17,6 +17,32 @@ from tenpy.networks import mps, mpo, site
 
 spin_half = site.SpinHalfSite(conserve='Sz')
 
+def check_hermitian(H):
+    """Check if `H` is a hermitian MPO."""
+    if not H.finite:
+        # include once over the boundary: double the unit cell
+        # a general MPO might have terms going over multiple unit cells, but we ignore that...
+        Ws = H._W * 2
+    else:
+        Ws = H._W
+    #check trace(H.H) = trace(H.H^dagger)
+    W = Ws[0].take_slice([H.get_IdL(0)], ['wL'])
+
+    trHH = npc.tensordot(W, W.replace_label('wR', 'wR*'), axes=[['p', 'p*'], ['p*', 'p']])
+    trHHd = npc.tensordot(W, W.conj(), axes=[['p', 'p*'], ['p*', 'p']])
+    for W in Ws[1:]:
+        trHH = npc.tensordot(trHH, W, axes=['wR', 'wL'])
+        trHHd = npc.tensordot(trHHd, W, axes=['wR', 'wL'])
+        trHH = npc.tensordot(trHH, W.replace_label('wR', 'wR*'),
+                             axes=[['wR*', 'p', 'p*'], ['wL', 'p*', 'p']])
+        trHHd = npc.tensordot(trHHd, W.conj(),
+                              axes=[['wR*', 'p', 'p*'], ['wL*', 'p*', 'p']])
+    i = H.get_IdR(H.L-1)
+    trHH = trHH[i, i]
+    trHHd = trHHd[i, i]
+    print "check_hermitian: ", trHH, trHHd
+    npt.assert_array_almost_equal_nulp(trHH, trHHd, H.L*20)
+
 
 def test_MPO():
     s = spin_half
