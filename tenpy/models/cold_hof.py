c6f7d793299f597a3152b12df1466c618991c293
Leon <leonschoonderwoerd@gmail.com>
2017-11-03 17:35:59 +0100

    More work on cold_hof.py (Lattice, setup for Hamiltonian).

--- tenpy/models/cold_hof.py
+++ tenpy/models/cold_hof.py
@@ -1,6 +1,7 @@
 """A TenPyLight implementation of the cold atomic (Harper-)Hofstadter model on a strip or cylinder.
 
 .. todo::
+	Sort out the lattice (including correct bc handling for cylinder/strip).
 	Implement the Hamiltonian.
 """
 
@@ -30,7 +31,7 @@ class cold_hof_model(CouplingModel, NearestNeighborModel, MPOModel):
     	maximum number of bosons per site
     filling : float
     	average number of bosons per site
-	Jx, Jy, phi_ext, kappa, omega, delta, u : float
+	Jx, Jy, phi_0, phi_ext, kappa, omega, delta, u, mu : float
 		Hamiltonian parameters
 	bc : {0 | 1}
 		boundary conditions along the circumference. 0 = open, 1 = periodic.
@@ -50,11 +51,13 @@ class cold_hof_model(CouplingModel, NearestNeighborModel, MPOModel):
     	filling = get_parameter('filling', 0.125, self.__class__)
     	Jx = get_parameter('Jx', 1., self.__class__)
     	Jy = get_parameter('Jy', 1., self.__class__)
+    	phi_0 = get_parameter('phi_0', 0., self.__class__)
     	phi_ext = get_parameter('phi_ext', 0, self.__class__)
     	kappa = get_parameter('kappa', 0, self.__class__)
     	omega = get_parameter('omega', 0, self.__class__)
     	delta = get_parameter('delta', 0, self.__class__)
     	u = get_parameter('u', 0, self.__class__)
+    	mu = get_parameter('mu', 0, self.__class__)
     	bc = get_parameter('bc', 1, self.__class__)  # = bc around the circumference
     	conserve = get_parameter('conserve', 'N', self.__class__)
     	verbose = get_parameter('verbose', 1, self.__class__)
@@ -64,5 +67,26 @@ class cold_hof_model(CouplingModel, NearestNeighborModel, MPOModel):
         site = BosonSite(Nmax=N_max, conserve='N', filling=filling)
 
         # 4) Lattice
-        lat = Lattice
+        lat = Lattice([Lx * mx, Ly * my], unit_cell, order='default', bc_MPS='finite', basis=None, positions=None)
+        bc_coupling = # TODO
 
+		# 5) initialize CouplingModel
+        CouplingModel.__init__(self, lat, bc_coupling)
+
+        # 6) add terms of the Hamiltonian
+        # TODO, self.add_onsite(), self.add_coupling()
+        self.add_onsite(u/2, 0, 'NN')
+        self.add_onsite((delta/2)*(-1), 0, 'N')  # Can we make this staggered?
+
+        hop_y = - Jy * ( 1 + mu * (kappa / (2 * omega)) ** 2 )
+        hop_x = - Jx * np.exp( 1.j * (np.pi / 2) * (index) - phi_0)  # TODO figure out index
+        hop_x_hc = - Jx * np.exp( 1.j * (np.pi / 2) * (index) - phi_0)  # TODO figure out index
+        self.add_coupling(hop_y, u1, 'B', u2, 'Bd', dx)  # TODO figure out u1, u2 and dx
+        self.add_coupling(hop_y, u1, 'Bd', u2, 'B', dx)
+        self.add_coupling(hop_x, u1, 'B', u2, 'Bd', dx)
+        self.add_coupling(hop_x_hc, u1, 'Bd', u2, 'B', dx)
+
+        # 7) initialize MPO
+        MPOModel.__init__(self, lat, self.calc_H_MPO())
+        # 8) initialize bonds (the order of 7/8 doesn't matter)
+        NearestNeighborModel.__init__(self, lat, self.calc_H_bond())
