2c9e41bada48a0393eff384bf299d50c7f55ce82
Johannes Hauschild <git@johannes-hauschild.de>
2019-02-17 22:28:37 +0100

    move tenpy/models/fermion_chain to tenpy/models/fermions_spinless.py
    keep the old file for backwards compatibility, issuing a warning.

--- tenpy/models/__init__.py
+++ tenpy/models/__init__.py
@@ -1,13 +1,10 @@
 """Definition of the various models.
 
-A general `model` represents an Hamiltonian and the Hilbert space it lives on.
-As such, it consits of
+For an introduction to models see :doc:`/intro_model`.
 
-1) the underlying lattice defining the local Hilbert space and onsite-operators
-2) couplings between two (or more) sites.
-
-MPS/MPO based algorithms like DMRG require to map this general model to a 1D-model with (possibly)
-long range order.
+The module :mod:`tenpy.models.model` contains base classes for models.
+The module :mod:`tenpy.models.lattice` contains base classes and implementations of lattices.
+All other modules in this folder contain model classed derived from these base classes.
 """
 # Copyright 2018 TeNPy Developers
 
