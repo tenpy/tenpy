2ec6fc720c05f6dfb4e34e6bc20ed2e849059aae
Johannes Hauschild <git@johannes-hauschild.de>
2019-03-07 16:13:53 +0100

    updated benchmark files

---
+++ tests/benchmark/split_npc.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import tenpy.linalg.np_conserved as npc
+
+import tenpy.tools.optimization as optimization
+import itertools as it
+
+import tensordot_npc
+
+
+def setup_benchmark(*args, **kwargs):
+    a, b, axes = tensordot_npc.setup_benchmark(*args, **kwargs)
+    axes_a, axes_b = axes
+    non_axes_a = [i for i in range(a.rank) if i not in axes_a]
+    non_axes_b = [i for i in range(b.rank) if i not in axes_b]
+    return a.combine_legs((non_axes_a, axes_a)), b.combine_legs((non_axes_b, axes_b))
+
+
+def benchmark(data):
+    a, b = data
+    a.split_legs([0, 1])
+    b.split_legs([0, 1])
+    return
