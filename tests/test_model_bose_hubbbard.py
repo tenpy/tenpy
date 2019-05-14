757c366bbda1cc6c73ce86796f51575fb64c7a08
tenpybot <tenpybot@johannes-hauschild.de>
2019-05-14 04:40:37 +0200

    Autoformat with yapf 0.27.0

--- tests/test_model_bose_hubbbard.py
+++ tests/test_model_bose_hubbbard.py
@@ -4,8 +4,13 @@ from test_model import check_general_model
 
 
 def test_BoseHubbardModel():
-    check_general_model(bose_hubbard.BoseHubbardModel,
-                        {'lattice': "Square", 'Lx': 2, 'Ly': 3, 'V': 0.1, 'U': 0.3}, {})
+    check_general_model(bose_hubbard.BoseHubbardModel, {
+        'lattice': "Square",
+        'Lx': 2,
+        'Ly': 3,
+        'V': 0.1,
+        'U': 0.3
+    }, {})
 
 
 def test_BoseHubbardChain():
