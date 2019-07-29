36b4297d8be3ae3d92f12a7135a113c1c4b2d994
Leon Schoonderwoerd <leonschoonderwoerd@gmail.com>
2019-07-29 11:49:54 +0100

    Revert toric code test; always compute LHeff/RHeff in OneSiteH

--- tenpy/algorithms/mps_sweeps.py
+++ tenpy/algorithms/mps_sweeps.py
@@ -554,18 +554,17 @@ class OneSiteH(EffectiveH):
         In a move to the right, we need LHeff. In a move to the left, we need RHeff. Both contain
         the same W.
         """
-        if self.move_right:
-            LHeff = npc.tensordot(self.LP, self.W, axes=['wR', 'wL'])
-            self.pipeL = pipeL = LHeff.make_pipe(['vR*', 'p'], qconj=+1)
-            self.LHeff = LHeff.combine_legs([['vR*', 'p'], ['vR', 'p*']],
-                                            pipes=[pipeL, pipeL.conj()],
-                                            new_axes=[0, 2])
-        else:
-            RHeff = npc.tensordot(self.W, self.RP, axes=['wR', 'wL'])
-            self.pipeR = pipeR = RHeff.make_pipe(['p', 'vL*'], qconj=-1)
-            self.RHeff = RHeff.combine_legs([['p', 'vL*'], ['p*', 'vL']],
-                                            pipes=[pipeR, pipeR.conj()],
-                                            new_axes=[-1, 0])
+        # Always compute both L/R, because we might need them. Could change later.
+        LHeff = npc.tensordot(self.LP, self.W, axes=['wR', 'wL'])
+        self.pipeL = pipeL = LHeff.make_pipe(['vR*', 'p'], qconj=+1)
+        self.LHeff = LHeff.combine_legs([['vR*', 'p'], ['vR', 'p*']],
+                                        pipes=[pipeL, pipeL.conj()],
+                                        new_axes=[0, 2])
+        RHeff = npc.tensordot(self.W, self.RP, axes=['wR', 'wL'])
+        self.pipeR = pipeR = RHeff.make_pipe(['p', 'vL*'], qconj=-1)
+        self.RHeff = RHeff.combine_legs([['p', 'vL*'], ['p*', 'vL']],
+                                        pipes=[pipeR, pipeR.conj()],
+                                        new_axes=[-1, 0])
 
 
 class TwoSiteH(EffectiveH):
