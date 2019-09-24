7e7411277a22df965cc0880f829eeb689578024d
ga27yem <samuel.scalet@tum.de>
2019-09-24 17:00:22 +0200

    Added apply_mpo()

--- tenpy/algorithms/mps_compress.py
+++ tenpy/algorithms/mps_compress.py
@@ -6,10 +6,11 @@ import numpy as np
 
 from ..linalg import np_conserved as npc
 from .truncation  import truncate
+from ..networks import mps
 
 
-def compress_MPS(psi, trunc_par):
-	r"""Takes an MPS and compresses it.
+def mps_compress(psi, trunc_par):
+	r"""Takes an MPS and compresses it. In Place.
 
 	Parameters
 	----------
@@ -18,6 +19,8 @@ def compress_MPS(psi, trunc_par):
 	trunc_par: dict
 		See :func:`truncate`
 	"""
+	if psi.bc != 'finite':
+		raise NotImplemented
 
 	# Do QR starting from the left
 	B=psi.get_B(0,form=None)
@@ -43,4 +46,53 @@ def compress_MPS(psi, trunc_par):
 		B.iscale_axis(s, 'vR')
 		psi.set_SL(i, s)
 	psi.set_B(0, B, form='B')
-	
+
+
+def apply_mpo(psi, mpo, trunc_par):
+    """Applies an mpo and truncates the resulting MPS using SVD.
+    
+    Parameters
+    ----------
+    mps: MPS
+        MPS to apply operator on
+    mpo: MPO
+        MPO to apply
+    trunc_par: dict
+        Truncation parameters. See :func:`truncate`
+    
+    Returns
+    -------
+    mps: MPS
+        Resulting new MPS
+    """
+    if psi.bc!='finite' or mpo.bc != 'finite':
+        raise ValueError("Only implemented for finite bc so far")
+    if psi.L != mpo.L:
+        raise ValueError("Length of MPS and MPO not the same")
+    Bs=[npc.tensordot(psi.get_B(i, form=None), mpo.get_W(i), axes=('p', 'p*')) for i in range(psi.L)]
+    for i in range(psi.L):
+        if i==0:
+            Bs[i]=Bs[i].take_slice(mpo.get_IdL(0) ,'wL')
+            Bs[i]=Bs[i].combine_legs(['wR','vR'], qconj=[-1])
+            Bs[i].ireplace_labels(['(wR.vR)'], ['vR'])
+            Bs[i].get_leg('vR').to_LegCharge()
+        elif i==psi.L-1:  
+            Bs[i]=Bs[i].take_slice(mpo.get_IdR(i) ,'wR')
+            Bs[i]=Bs[i].combine_legs(['wL','vL'], qconj=[1])
+            Bs[i].ireplace_labels(['(wL.vL)'], ['vL'])
+            Bs[i].get_leg('vL').to_LegCharge()
+        else:
+            Bs[i]=Bs[i].combine_legs([['wL','vL'],['wR','vR']], qconj=[+1, -1])
+            Bs[i].ireplace_labels(['(wL.vL)', '(wR.vR)'], ['vL','vR'])
+            Bs[i].get_leg('vL').to_LegCharge()
+            Bs[i].get_leg('vR').to_LegCharge()
+    
+    #Wrong S values but will be calculated in mps_compress
+    S=[np.ones(1)]
+    for i in range(psi.L-1):
+        S.append(np.ones(Bs[i].shape[Bs[i].get_leg_index('vR')]))
+    S.append(np.ones(1))
+    new_mps = mps.MPS(psi.sites, Bs, S, form=None)
+    mps_compress(new_mps, trunc_par)
+    return new_mps
+    
