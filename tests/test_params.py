ef7590a70064921857b9d193054d623d54661be1
Leon Schoonderwoerd <leonschoonderwoerd@gmail.com>
2020-03-30 14:28:22 +0100

    Switch test_params.py to Parameters class.

--- tests/test_params.py
+++ tests/test_params.py
@@ -2,25 +2,26 @@
 # Copyright 2018-2020 TeNPy Developers, GNU GPLv3
 
 import warnings
-from tenpy.tools import params
+from tenpy.tools.params import Parameters
 
 
 def example_function(example_pars, keys=['a', 'b']):
     """example function using a parameter dictionary."""
     for default, k in enumerate(keys):
-        p_k = params.get_parameter(example_pars, k, default, "testing")
+        p_k = example_pars.get(k, default)
         print("read out parameter {p_k!r}".format(p_k=p_k))
 
 
-def test_paramters():
-    pars = dict()
+def test_parameters():
+    pars = Parameters(dict(), "Test empty")
     example_function(pars)
-    pars = dict(a=None, b=2.5, c="non-used")
+    pars = Parameters(dict(a=None, b=2.5, c="dict-style", d="non-used"), "Test parameters")
+    assert pars['c'] == "dict-style"
     example_function(pars)
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter('always')
-        pars = dict(miss_spelled=1.23)
+        pars = Parameters(dict(miss_spelled=1.23), "Test unused")
         example_function(pars)
-        unused = params.unused_parameters(pars, "testing")
+        unused = pars.unused
         assert len(unused) == 1
         assert len(w) == 1
