991c8f614694f85fa630e7572f921e13857e77b1
Johannes Hauschild <git@johannes-hauschild.de>
2020-04-23 23:43:01 -0700

    require sphinx >= 3.0

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ except:
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '2.0'
+needs_sphinx = '3.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
