8382bf3ef994c9152af03b75696acfb7f35fdd24
wilhelm.kadow <wilhelm.kadow@gmail.com>
2020-06-16 13:34:52 +0200

    correlation tests in test_time_evolution.py

--- tests/test_time_evolution.py
+++ tests/test_time_evolution.py
@@ -58,7 +58,9 @@ def exact_expectation(L, g, t=1., dt=0.01):
     for i in range(L):
         U[:, L+i] = gamma@U[:, i]
 
-    mag = []
+    mag = [] #avarage magnetization
+    szsz = [] #correlation functions
+    spsm = [] # nearest neighbor <S+S- + S-S+> correlation
     for t in np.arange(0,t,dt):
         Ub = U@np.diag(np.exp(-1j*t*v))@U.conj().T@U0 #the total (unitary) Boguliobov transformation
         X = Ub[L::, L::]
@@ -86,21 +88,45 @@ def exact_expectation(L, g, t=1., dt=0.01):
             assert abs(ni.imag) < 1e-10
             mz += 1-2*ni.real
         mag.append(mz/L)
-    return np.array(mag)
 
-def test_time_methods():
+        #measure sigmaz-sigmaz correlations
+        i,j = 0, 5
+        ninj =  -0.25*(-Delta[j+L, i+3*L] + Delta[i+L, j+3*L])*(-Delta[j+2*L, i] + Delta[i+2*L, j]) \
+                +0.25*(-Delta[j+L, j] + Delta[j+2*L, j+3*L])*(-Delta[i+L, i] + Delta[i+2*L, i+3*L]) \
+                -0.25*(-Delta[j+L, i] + Delta[i+2*L, j+3*L])*(-Delta[i+L, j] + Delta[j+2*L, i+3*L])
+        ni = Delta[i+L, i]
+        nj = Delta[j+L, j]
+        s = 1-2*ni-2*nj+4*ninj
+        assert abs(s.imag) < 1e-10
+        szsz.append(s.real)
+
+        #measure sigma+sigma- correlatios
+        s = 0
+        for i in range(L-1):
+            j = i+1
+            ci = 0.5*(Delta[j+L, i] - Delta[i+2*L, j+3*L] + Delta[i+L, j] - Delta[j+2*L, i+3*L])
+            assert abs(ci.imag) < 1e-10
+            s += ci.real
+        spsm.append(s/(L-1))
+    return np.array(mag), np.array(szsz), np.array(spsm)
+
+@pytest.mark.parametrize('algorithm', [
+    'TEBD', 'TDVP'
+])
+def test_time_methods(algorithm):
     L=6
     g=1.5
 
     model_params = dict(L=L, J=1., g=g, bc_MPS='finite', conserve=None)
     M = TFIChain(model_params)
     product_state = ["up"] * L #prepare system in spin polarized state
-    psi_tebd = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
-    psi_tdvp = psi_tebd.copy()   
+    psi = MPS.from_product_state(M.lat.mps_sites(), product_state, bc=M.lat.bc_MPS)
 
     dt = 0.01
     N_steps = 2
-    tebd_params = {
+    t = 0.5 #total time evolution
+    params = {
+        'active_sites': 2,
         'order': 2,
         'dt': dt,
         'N_steps': N_steps,
@@ -110,30 +136,30 @@ def test_time_methods():
             'trunc_cut': None
         }
     }
-    tdvp_params = {
-        'start_time': 0,
-        'dt': dt,
-        'trunc_params': {
-            'chi_max': 50,
-            'svd_min': 1.e-10,
-            'trunc_cut': None
-        }
-    }
-    engine_tebd = tebd.Engine(psi_tebd, M, tebd_params)
-    engine_tdvp = tdvp.Engine(psi_tdvp, M, tdvp_params)
-    m_tebd = [np.mean(psi_tebd.expectation_value("Sigmaz"))]
-    m_tdvp = [np.mean(psi_tdvp.expectation_value("Sigmaz"))]
-    t = 0.5
+    if algorithm == 'TEBD':
+        eng = tebd.Engine(psi, M, params)
+    elif algorithm == 'TDVP':
+        eng = tdvp.Engine(psi, M, params)
+    else:
+        raise ValueError("test works only for TEDB and TDVP so far")
+
+    mag = [np.mean(psi.expectation_value("Sigmaz"))]
+    i,j = 0, 5
+    szsz = [psi.correlation_function("Sigmaz", "Sigmaz", [i], [j])[0,0]]
+    spsm = [np.mean(psi.correlation_function("Sp", "Sm").diagonal(1)+psi.correlation_function("Sp", "Sm").diagonal(-1))]
+    
     for ti in np.arange(0, t, dt*N_steps):
-        engine_tebd.run()
-        engine_tdvp.run_two_sites(N_steps=N_steps)
+        eng.run()
 
-        m_tebd.append(np.mean(psi_tebd.expectation_value("Sigmaz")))
-        m_tdvp.append(np.mean(psi_tdvp.expectation_value("Sigmaz")))
-
-    m_exact = exact_expectation(L, g, t, tebd_params['dt']*tebd_params['N_steps'])
-    npt.assert_almost_equal(np.array(m_tebd)[:-1], m_exact, 4)
-    npt.assert_almost_equal(np.array(m_tdvp)[:-1], m_exact, 4)
+        mag.append(np.mean(psi.expectation_value("Sigmaz")))
+        i,j = 0, 5
+        szsz.append(psi.correlation_function("Sigmaz", "Sigmaz", [i], [j])[0,0])
+        spsm.append(np.mean(psi.correlation_function("Sp", "Sm").diagonal(1)+psi.correlation_function("Sp", "Sm").diagonal(-1)))
+    
+    m_exact, szsz_exact, spsm_exact = exact_expectation(L, g, t, dt*N_steps)
+    npt.assert_almost_equal(np.array(mag)[:-1], m_exact, 4)
+    npt.assert_almost_equal(np.array(szsz)[:-1], szsz_exact, 4)
+    npt.assert_almost_equal(np.array(spsm)[:-1], spsm_exact, 4)
 
     #TODO add MPO evolution
     #TODO add infinite MPS (?)
