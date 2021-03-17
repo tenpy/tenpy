c73c0086ffb3d552e4cd7cd7e0afc1003153d4e7
Johannes Hauschild <git@johannes-hauschild.de>
2021-03-16 18:54:09 -0700

    Revert "revert merging the MPI_parallel_plus_hc branch into main:"
    This reverts commit dcf8941d902bc5c0d9dfaa9512d1f1022662e880.
    
    Development on MPI should continue from here!

--- tenpy/simulations/__init__.py
+++ tenpy/simulations/__init__.py
@@ -11,11 +11,14 @@ The classes provided here provide a structure for the whole setup of simulations
     measurement
     ground_state_search
     time_evolution
+    mpi_parallel
 
 """
 # Copyright 2020-2021 TeNPy Developers, GNU GPLv3
 
 from . import measurement, simulation, ground_state_search, time_evolution
+
+# don'te import mpi_parallel : it imports mpi4py, which has side effects!
 
 __all__ = [
     "simulation",
