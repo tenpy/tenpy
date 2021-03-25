d7fcdea7f077edea0e3dbe9bb0722f883cdff638
Johannes Hauschild <git@johannes-hauschild.de>
2021-03-25 12:14:51 -0700

    reset released=False

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if not sys.version_info >= (3, 6):
 # hardcode version for people without git
 
 MAJOR, MINOR, MICRO = 0, 8, 2
-RELEASED = True
+RELEASED = False
 VERSION = '{0:d}.{1:d}.{2:d}'.format(MAJOR, MINOR, MICRO)
 
 #  Before updating a version, make sure that *all* tests run successfully!
