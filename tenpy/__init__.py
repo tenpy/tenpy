3518763b4e998a359942983fea6c6f7e08707b65
Johannes Hauschild <git@johannes-hauschild.de>
2021-04-16 14:06:35 -0700

    fix: allow to run from console with output_filename_params

--- tenpy/__init__.py
+++ tenpy/__init__.py
@@ -88,7 +88,7 @@ def console_main():
             set_recursive(options, key, val, insert_dicts=True)
     if len(options) == 0:
         raise ValueError("No options supplied! Check your command line arguments!")
-    if 'output_filename' not in options:
+    if 'output_filename' not in options and 'output_filename_params' not in options:
         raise ValueError("No output filename specified - refuse to run without saving anything!")
     if 'sequential' not in options:
         run_simulation(args.sim_class, **options)
