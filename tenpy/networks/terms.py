5aab23be1aedbf33fbf2544c40ebe75c31924a8c
Wilhelm Kadow <wilhelm.kadow@gmail.com>
2021-04-19 15:45:29 +0200

    new structure for multicoupling

--- tenpy/networks/terms.py
+++ tenpy/networks/terms.py
@@ -816,43 +816,50 @@ class CouplingTerms(Hdf5Exportable):
 
 class MultiCouplingTerms(CouplingTerms):
     """Operator names, site indices and strengths representing general `M`-site coupling terms.
-
     Generalizes the :attr:`coupling_terms` of :class:`CouplingTerms` to `M`-site couplings.
     The structure of the nested dictionary :attr:`coupling_terms` is similar, but we allow
-    an arbitrary recursion depth of the dictionary.
-
+    an arbitrary recursion depth of the dictionary and build from the left and right
+    simultaneously.
     Parameters
     ----------
     L : int
         Number of sites.
-
     Attributes
     ----------
     L : int
         Number of sites.
+    counter: int
+        Counts the number of couplings, start with L to avoid confusion with site numbering.
     coupling_terms : dict of dict
-        Nested dictionaries of the following form::
-
-            {i: {('opname_i', 'opname_string_ij'):
-                    {j: {('opname_j', 'opname_string_jk'):
-                            {k: {('opname_k', 'opname_string_kl'):
-                                ...
-                                    {l: {'opname_l':
-                                            strength
-                                    }   }
-                                ...
-                            }   }
-                    }   }
-            }   }
-
+        Nested dictionaries of the following form for left and right::
+            
+        left = {ijkl[0]: {(ops_ijkl[0], op_string[0]):
+                    {ijkl[1]: {(ops_ijkl[1], op_string[1]):
+                              ...
+                                  {ijkl[switchLR]: {(ops_ijkl[switchLR],  op_string[switchLR]): 
+                                                    {counter: strength}}}
+                    }         }
+         }         }
+         right = {ijkl[-1]: {(ops_ijkl[-1], op_string[-1]):
+                    {ijkl[-2]: {(ops_ijkl[-2], op_string[-2]):
+                               ...
+                                   {ijkl[switchLR+1]: {(ops_ijkl[switchLR+1],  op_string[switchLR+1]): 
+                                                     {counter: switchLR}}}
+                    }         }
+         }         }
         For a M-site coupling, this involves a nesting depth of ``2*M`` dictionaries.
         Note that always ``i < j < k < ... < l``, but entries with ``j,k,l >= L``
         are allowed for the case of ``bc_MPS == 'infinite'``, when they indicate couplings
         between different iMPS unit cells.
     """
-    def add_multi_coupling_term(self, strength, ijkl, ops_ijkl, op_string="Id"):
+    def __init__(self, L):
+        assert L > 0
+        self.L = L
+        self.coupling_terms = (dict(),dict()) #left and right dictionary
+        self.counter = L #counts the number of couplings, start with L to avoid confusion with site numbering
+        
+    def add_multi_coupling_term(self, strength, ijkl, ops_ijkl, op_string="Id", switchLR=None):
         """Add a multi-site coupling term.
-
         Parameters
         ----------
         strength : float
@@ -868,6 +875,10 @@ class MultiCouplingTerms(CouplingTerms):
             Names of the operator to be inserted between the operators,
             e.g., op_string[0] is inserted between `i` and `j`.
             A single name holds for all in-between segments.
+        switchLR: int
+            The site where we switch from building the coupling from the left to building the
+            coupling from the right for an efficient MPO representation.
+            Default is (i+l)/2
         """
         if len(ijkl) < 2:
             raise ValueError("Need to act on at least 2 sites. Use onsite terms!")
@@ -877,26 +888,45 @@ class MultiCouplingTerms(CouplingTerms):
         for i, j in zip(ijkl, ijkl[1:]):
             if not i < j:
                 raise ValueError("Need i < j < k < ...")
-        # create the nested structure
-        # {ijkl[0]: {(ops_ijkl[0], op_string[0]):
+        if switchLR is None:
+            switchLR = (ijkl[0]+ijkl[-1])//2
+        assert switchLR < ijkl[-1]
+        # create nested structures from the left and right
+        # left = {ijkl[0]: {(ops_ijkl[0], op_string[0]):
         #            {ijkl[1]: {(ops_ijkl[1], op_string[1]):
         #                       ...
-        #                           {ijkl[-1]: {ops_ijkl[-1]: strength}
+        #                           {ijkl[switchLR]: {(ops_ijkl[switchLR],  op_string[switchLR]): 
+        #                                             {counter: strength}}
         #            }         }
         # }         }
-        d0 = self.coupling_terms
+        # right = {ijkl[-1]: {(ops_ijkl[-1], op_string[-1]):
+        #            {ijkl[-2]: {(ops_ijkl[-2], op_string[-2]):
+        #                       ...
+        #                           {ijkl[switchLR+1]: {(ops_ijkl[switchLR+1],  op_string[switchLR+1]): 
+        #                                             {counter: switchLR}}
+        #            }         }
+        # }         }
+        d0L, d0R = self.coupling_terms
+        #add left terms
         for i, op, op_str in zip(ijkl, ops_ijkl, op_string):
-            d1 = d0.setdefault(i, dict())
-            d0 = d1.setdefault((op, op_str), dict())
-        d1 = d0.setdefault(ijkl[-1], dict())
-        op = ops_ijkl[-1]
-        d1[op] = d1.get(op, 0) + strength
+            if i <= switchLR:
+                d1L = d0L.setdefault(i, dict())
+                d0L = d1L.setdefault((op, op_str), dict())
+        d0L[self.counter] = strength
 
+        #add right terms
+        for i, op, op_str in zip(reversed(ijkl), reversed(ops_ijkl), reversed(op_string)):
+            if i > switchLR:
+                d1R = d0R.setdefault(i, dict())
+                d0R = d1R.setdefault((op, op_str), dict())
+                
+        if switchLR < ijkl[-1]:
+            d0R[self.counter] = switchLR
+        self.counter += 1
+        
     def multi_coupling_term_handle_JW(self, strength, term, sites, op_string=None):
         """Helping function to call before :meth:`add_multi_coupling_term`.
-
         Handle/figure out Jordan-Wigner strings if needed.
-
         Parameters
         ----------
         strength : float
@@ -912,14 +942,11 @@ class MultiCouplingTerms(CouplingTerms):
         op_string : None | str
             Operator name to be used as operator string *between* the operators, or ``None`` if the
             Jordan Wigner string should be figured out.
-
             .. warning ::
-
                 ``None`` figures out for each segment between the operators, whether a
                 Jordan-Wigner string is needed.
                 This is different from a plain ``'JW'``, which just applies a string on
                 each segment!
-
         Returns
         -------
         strength, ijkl, ops_ijkl, op_string :
@@ -965,155 +992,143 @@ class MultiCouplingTerms(CouplingTerms):
         return strength, ijkl, ops, new_op_str
 
     def max_range(self):
+        #return NotImplementedError
         """Determine the maximal range in :attr:`coupling_terms`.
-
         Returns
         -------
         max_range : int
             The maximum of ``j - i`` for the `i`, `j` occuring in a term of :attr:`coupling_terms`.
         """
-        max_range = 0
-        for i, d1 in self.coupling_terms.items():
-            max_range = max(max_range, self._max_range(i, d1))
-        return max_range
-
-    def add_to_graph(self, graph, _i=None, _d1=None, _label_left=None):
+        d0L, d0R = self.coupling_terms
+        return max(d0R.keys())-min(d0L.keys())
+    
+    def add_to_graph(self, graph):
         """Add terms from :attr:`coupling_terms` to an MPOGraph.
-
         Parameters
         ----------
         graph : :class:`~tenpy.networks.mpo.MPOGraph`
             The graph into which the terms from :attr:`coupling_terms` should be added.
-        _i, _d1, _label_left : None
-            Should not be given; only needed for recursion.
         """
         assert self.L == graph.L
-        # nested structure of coupling_terms:
-        # d0 = {i: {('opname_i', 'opname_string_ij'): ... {l: {'opname_l': strength}}}
+        connect = self._add_from_left(graph) #returns a dictionary to connect left and right graph
+        self._add_from_right(graph, connect)
+
+    def _add_from_left(self, graph, _i=None, _d1=None, _label_left=None, connect={}):
         if _i is None:  # beginning of recursion
-            for i, d1 in self.coupling_terms.items():
-                self.add_to_graph(graph, i, d1, 'IdL')
+            for i, d1 in self.coupling_terms[0].items():
+                connect = self._add_from_left(graph, i, d1, 'IdL', connect)
         else:
             for key, d2 in _d1.items():
-                if isinstance(key, tuple):  # further nesting
-                    op_i, op_string_ij = key
-                    if isinstance(_label_left, str) and _label_left == 'IdL':
-                        label = (_i, op_i, op_string_ij)
-                    else:
-                        label = _label_left + (_i, op_i, op_string_ij)
-                    graph.add(_i, _label_left, label, op_i, 1., skip_existing=True)
-                    for j, d3 in d2.items():
-                        label_j = graph.add_string(_i, j, label, op_string_ij)
-                        self.add_to_graph(graph, j, d3, label_j)
-                else:  # maximal nesting reached: exit recursion
-                    # i is actually the `l`
-                    op_i, strength = key, d2
-                    graph.add(_i, _label_left, 'IdR', op_i, strength)
-        # done
-
-    def remove_zeros(self, tol_zero=1.e-15, _d0=None):
-        """Remove entries close to 0 from :attr:`coupling_terms`.
-
-        Parameters
-        ----------
-        tol_zero : float
-            Entries in :attr:`coupling_terms` with `strength` < `tol_zero` are considered to be
-            zero and removed.
-        _d0 : None
-            Should not be given; only needed for recursion.
-        """
-        if _d0 is None:
-            _d0 = self.coupling_terms
-        # d0 = ``{i: {('opname_i', 'opname_string_ij'): ... {j: {'opname_j': strength}}}``
-        for i, d1 in list(_d0.items()):
-            for key, d2 in list(d1.items()):
-                if isinstance(key, tuple):
-                    self.remove_zeros(tol_zero, d2)  # recursive!
-                    if len(d2) == 0:
-                        del d1[key]
+                op_i, op_string_ij = key
+                if isinstance(_label_left, str) and _label_left == 'IdL':
+                    label = (_i, op_i, op_string_ij)
                 else:
-                    # key is opname_j, d2 is strength
-                    if abs(d2) < tol_zero:
-                        del d1[key]
-            if len(d1) == 0:
-                del _d0[i]
-        # done
+                    label = _label_left + (_i, op_i, op_string_ij)
+                graph.add(_i, _label_left, label, op_i, 1., skip_existing=True)
+                for j, d3 in d2.items():
+                    if isinstance(d3, dict):  # further nesting
+                        label_j = graph.add_string(_i, j, label, op_string_ij)
+                        connect = self._add_from_left(graph, j, d3, label_j, connect)
+                    else:  #exit recursion
+                        connect[j] = (d3, label)
+        return connect
+
+    def _add_from_right(self, graph, connect, _i=None, _d1=None, _label_right=None):
+        if _i is None:  # beginning of recursion
+            for i, d1 in self.coupling_terms[1].items():
+                self._add_from_right(graph, connect, i, d1, 'IdR')
+        else:
+            for key, d2 in _d1.items():
+                op_i, op_string_ij = key
+                if isinstance(_label_right, str) and _label_right == 'IdR':
+                    label = (_i, op_i, op_string_ij)
+                else:
+                    label = _label_right + (_i, op_i, op_string_ij)
+                for j, d3 in d2.items():
+                    if isinstance(d3, dict):  # further nesting
+                        graph.add(_i, label, _label_right, op_i, 1., skip_existing=True)
+                        label_j = graph.add_string(j, _i, label, op_string_ij)
+                        self._add_from_right(graph, connect, j, d3, label_j)
+                    else: #exit recursion
+                        strength, label_left = connect[j]
+                        switchLR = d3 #from the construction of the coupling_terms
+                        if _i == switchLR+1:
+                            label_2 = graph.add_string(label_left[-3], _i, label_left, op_string_ij)
+                            graph.add(_i, label_2, _label_right, op_i, strength, skip_existing=False)
+                        else:
+                            graph.add(_i, label, _label_right, op_i, 1., skip_existing=True)
+                            label_2 = graph.add_string(label_left[-3], switchLR+1, label_left, op_string_ij)
+                            label_3 = graph.add_string(switchLR+1, _i, label, op_string_ij)
+                            graph.add(switchLR+1, label_2, label_3, op_string_ij, strength, skip_existing=False)
+        
+    def remove_zeros(self, tol_zero=1.e-15, _d0=None):
+        #not implemented yet
+        return
+        #raise NotImplementedError
 
     def to_TermList(self):
-        """Convert :attr:`onsite_terms` into a :class:`TermList`.
-
-        Returns
-        -------
-        term_list : :class:`TermList`
-            Representation of the terms as a list of terms.
-        """
-        terms = []
-        strength = []
-        self._to_TermList(terms, strength, [], self.coupling_terms)
-        return TermList(terms, strength)
+        raise NotImplementedError
 
     def __iadd__(self, other):
         if not isinstance(other, CouplingTerms):
             return NotImplemented  # unknown type of other
         if other.L != self.L:
             raise ValueError("incompatible lengths")
-        self._iadd_multi_coupling_terms(self.coupling_terms, other.coupling_terms)
+        if not isinstance(other, MultiCouplingTerms):
+            #transform coupling to multi coupling
+            for i, d0 in other.coupling_terms.items():
+                for op_i, d1 in d0.items():
+                    for j, d2 in d1.items():
+                        for op_j, strength in d2.items():
+                            ijkl = [i,j]
+                            ops_ijkl = [op_i[0], op_j]
+                            self.add_multi_coupling_term(strength, ijkl, ops_ijkl, op_i[1])
+        else: # add multi coupling to the left and right dictionaries  
+            nc = self._iadd_multi_left(self.coupling_terms[0], other.coupling_terms[0])
+            self._iadd_multi_right(self.coupling_terms[1], other.coupling_terms[1], nc)
         return self
 
-    def _max_range(self, i, d1):
-        max_range = 0
-        for key, d2 in d1.items():
-            if isinstance(key, tuple):
-                # further couplings: d2 is dictionary
-                j_max = max(d2.keys())
-                max_range = max(max_range, j_max - i)
-                for d3 in d2.values():
-                    max_range = max(max_range, self._max_range(i, d3))
-            # else: d2 is strength, last coupling reached
-        return max_range
-
     def _to_TermList(self, terms, strength, term0, d0):
-        for i in sorted(d0):
-            d1 = d0[i]
-            d1_keys_tuple = []
-            d1_keys_str = []
-            for key in d1.keys():
-                if isinstance(key, tuple):
-                    d1_keys_tuple.append(key)
-                else:
-                    d1_keys_str.append(key)
-            for opname_i in sorted(d1_keys_str):
-                # maximum recursion reached
-                terms.append(term0 + [(opname_i, i)])
-                strength.append(d1[opname_i])
-            for key in sorted(d1_keys_tuple):
-                (opname_i, op_str) = key
-                term1 = term0 + [(opname_i, i)]
-                d2 = d1[key]
-                self._to_TermList(terms, strength, term1, d2)
-        # done
-
-    def _iadd_multi_coupling_terms(self, self_d0, other_d0):
-        # {ijkl[0]: {(ops_ijkl[0], op_string[0]):
-        #            {ijkl[1]: {(ops_ijkl[1], op_string[1]):
-        #                       ...
-        #                           {ijkl[-1]: {ops_ijkl[-1]: strength}
-        #            }         }
-        # }         }
-        # d0 = ``{i: {('opname_i', 'opname_string_ij'): ... {j: {'opname_j': strength}}}``
-        for i, other_d1 in other_d0.items():
-            self_d1 = self_d0.setdefault(i, dict())
-            for key, other_d2 in other_d1.items():
-                if isinstance(key, tuple):  # further couplings
-                    self_d2 = self_d1.setdefault(key, dict())
-                    self._iadd_multi_coupling_terms(self_d2, other_d2)  # recursive!
-                else:  # last term of the coupling
-                    opname_j = key
-                    strength = other_d2
-                    self_d1[opname_j] = self_d1.get(opname_j, 0.) + strength
-        # done
+        raise NotImplementedError
+        
+    def _iadd_multi_left(self, self_d0, other_d0, new_counter=None):
+        #add terms in the left dictionary
+        if new_counter is None: #begin recursion
+            new_counter = {} #new_counter[other_counter] = new counter in self
+            for i, other_d1 in other_d0.items():
+                self_d1 = self_d0.setdefault(i, dict())
+                new_counter = self._iadd_multi_left(self_d1, other_d1, new_counter)
+        else:
+            for key, other_d1 in other_d0.items():
+                self_d1 = self_d0.setdefault(key, dict())
+                for j, other_d2 in other_d1.items():
+                    if isinstance(other_d2, dict):  # further nesting
+                        self_d2 = self_d1.setdefault(j, dict())
+                        new_counter = self._iadd_multi_left(self_d2, other_d2, new_counter)  #recursive
+                    else:  #exit recurison
+                        self_d1[self.counter] = other_d1[j] # = strength
+                        new_counter[j] = self.counter
+                        self.counter += 1
+        return new_counter
+    
+    def _iadd_multi_right(self, self_d0, other_d0, new_counter, recursion=False):
+        #add terms in the right dictionary
+        if not recursion: #begin recursion
+            for i, other_d1 in other_d0.items():
+                self_d1 = self_d0.setdefault(i, dict())
+                self._iadd_multi_right(self_d1, other_d1, new_counter, True)
+        else:
+            for key, other_d1 in other_d0.items():
+                self_d1 = self_d0.setdefault(key, dict())
+                for j, other_d2 in other_d1.items():
+                    if isinstance(other_d2, dict):  # further nesting
+                        self_d2 = self_d1.setdefault(j, dict())
+                        self._iadd_multi_right(self_d2, other_d2, new_counter, True)  #recursive
+                    else:  #exit recursion
+                        self_d1[new_counter[j]] = other_d1[j] # = switchLR
 
     def _test_terms(self, sites, d0=None):
+        raise NotImplementedError
         N_sites = len(sites)
         if d0 is None:
             d0 = self.coupling_terms
@@ -1129,7 +1144,6 @@ class MultiCouplingTerms(CouplingTerms):
                     op_i = key
                     if not site_i.valid_opname(op_i):
                         raise ValueError("Operator {op!r} not in site".format(op=op_i))
-        # done
 
 
 class ExponentiallyDecayingTerms(Hdf5Exportable):
