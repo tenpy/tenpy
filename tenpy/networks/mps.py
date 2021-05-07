tenpy/tenpy

commit c4804b85abcc620934d4464000beab1f7bd35810

replace some UserWarnings with logger.warn.
psi.canonical_form_infinite: warn only about significant drop in chi

tenpy/networks/mps.py
@@ -3668,10 +3668,9 @@ class MPS:
             W = -W  # should actually never happen:  we initially normalize tr(Gr) = chi > 0
         # discard small values on order of machine precision
         proj = (W > eps)
-        if np.count_nonzero(proj) < len(W):
+        if np.count_nonzero(proj) < len(W) * 0.9:
             # project into non-degenerate subspace, reducing the bond dimensions!
-            warnings.warn("canonical_form_infinite: project to smaller bond dimension",
-                          stacklevel=3)
+            logger.warn("canonical_form_infinite: project to significantly smaller bond dimension")
             XH.iproject(proj, axes=1)
             W = W[proj]
         norm = len(W) / np.sum(W)
@@ -3710,10 +3709,9 @@ class MPS:
         s_norm = 1.
         # discard small values on order of machine precision
         proj = (S2 > eps)
-        if np.count_nonzero(proj) < len(S2):
+        if np.count_nonzero(proj) < len(S2) * 0.9:
             # project into non-degenerate subspace, reducing the bond dimensions!
-            warnings.warn("canonical_form_infinite: project to smaller bond dimension",
-                          stacklevel=2)
+            logger.warn("canonical_form_infinite: project to significantly smaller bond dimension")
             YH.iproject(proj, axes=1)
             S2 = S2[proj]
             s_norm = np.sqrt(np.sum(S2))
@@ -3873,7 +3871,7 @@ class MPSEnvironment:
                 init_LP.get_leg('vR').test_contractible(self.ket.get_theta(i0, 1).get_leg('vL'))
                 init_LP.get_leg('vR*').test_equal(self.bra.get_theta(i0, 1).get_leg('vL'))
             except ValueError:
-                warnings.warn("dropping `init_LP` with incompatible legs")
+                logger.warn("dropping `init_LP` with incompatible legs")
                 init_LP = None
         if init_RP is not None:
             try:
@@ -3881,7 +3879,7 @@ class MPSEnvironment:
                 init_RP.get_leg('vL').test_contractible(self.ket.get_theta(j0).get_leg('vR'))
                 init_RP.get_leg('vL*').test_equal(self.bra.get_theta(j0).get_leg('vR'))
             except ValueError:
-                warnings.warn("dropping `init_RP` with incompatible legs")
+                logger.warn("dropping `init_RP` with incompatible legs")
                 init_RP = None
         if init_LP is None:
             init_LP = self.init_LP(0, start_env_sites)

tenpy/networks/mpo.py
@@ -40,6 +40,8 @@ import numpy as np
 from scipy.linalg import expm
 import warnings
 import sys
+import logging
+logger = logging.getLogger(__name__)
 
 from ..linalg import np_conserved as npc
 from .site import group_sites, Site
@@ -1768,7 +1770,7 @@ class MPOEnvironment(MPSEnvironment):
         if start_env_sites is None:
             start_env_sites = 0 if self._finite else self.H.max_range
             if start_env_sites is None or start_env_sites > self.L:
-                warnings.warn("reducing default `start_env_sites` to L")
+                logger.warn("reducing default `start_env_sites` to L")
                 start_env_sites = self.L
         if self._finite and start_env_sites != 0:
             warnings.warn("setting `start_env_sites` to 0 for finite MPS")
@@ -1777,14 +1779,14 @@ class MPOEnvironment(MPSEnvironment):
             try:
                 init_LP.get_leg('wR').test_contractible(self.H.get_W(0).get_leg('wL'))
             except ValueError:
-                warnings.warn("dropping `init_LP` with incompatible legs")
+                logger.warn("dropping `init_LP` with incompatible legs")
                 init_LP = None
         if init_RP is not None:
             try:
                 j = self.L - 1
                 init_RP.get_leg('wL').test_contractible(self.H.get_W(j).get_leg('wR'))
             except ValueError:
-                warnings.warn("dropping `init_RP` with incompatible legs")
+                logger.warn("dropping `init_RP` with incompatible legs")
                 init_RP = None
         super().init_first_LP_last_RP(init_LP, init_RP, age_LP, age_RP, start_env_sites)
 

commit 0e49978d198cad30cb7fd0cbe39a1074e8e84d31

fix bug introduced in c4804b85abcc620934d4464000beab1f7bd35810
The referenced commit intended to only change warning verbosity,
but actually dropped some fixes to avoid numerical instabilities.

tenpy/networks/mps.py
@@ -3809,9 +3809,10 @@ class MPS:
             W = -W  # should actually never happen:  we initially normalize tr(Gr) = chi > 0
         # discard small values on order of machine precision
         proj = (W > eps)
-        if np.count_nonzero(proj) < len(W) * 0.9:
+        if np.count_nonzero(proj) < len(W):
             # project into non-degenerate subspace, reducing the bond dimensions!
-            logger.warn("canonical_form_infinite: project to significantly smaller bond dimension")
+            if np.count_nonzero(proj) < len(W) * 0.9:
+                logger.warn("canonical_form_infinite: project to significantly smaller chi")
             XH.iproject(proj, axes=1)
             W = W[proj]
         norm = len(W) / np.sum(W)
@@ -3847,9 +3848,10 @@ class MPS:
         s_norm = 1.
         # discard small values on order of machine precision
         proj = (S2 > eps)
-        if np.count_nonzero(proj) < len(S2) * 0.9:
+        if np.count_nonzero(proj) < len(S2):
             # project into non-degenerate subspace, reducing the bond dimensions!
-            logger.warn("canonical_form_infinite: project to significantly smaller bond dimension")
+            if np.count_nonzero(proj) < len(W) * 0.9:
+                logger.warn("canonical_form_infinite: project to significantly smaller chi")
             YH.iproject(proj, axes=1)
             S2 = S2[proj]
             s_norm = np.sqrt(np.sum(S2))