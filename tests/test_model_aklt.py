43c50a6779c39f87fa7f652afa1f24d52cdc1e57
Johannes Hauschild <git@johannes-hauschild.de>
2021-06-02 19:59:18 -0700

    test of aklt model: ambiguity of psi!

--- tests/test_model_aklt.py
+++ tests/test_model_aklt.py
@@ -12,4 +12,7 @@ def test_AKLT(L=4):
     eng = TwoSiteDMRGEngine(psi, M, {'trunc_params': {'svd_min': 1.e-6}})
     E0, psi0 = eng.run()
     assert abs(E0 - (-2 / 3.) * (L - 1)) < 1.e-10
-    assert psi0.chi == [2, 2, 2]
+    # note: if we view the system as spin-1/2 projected to spin-1, the first and last spin
+    # are arbitrary.
+    # if they are in a superposition, this will contribute at most another factor of 2 to chi.
+    assert all([chi <= 4 for chi in psi0.chi])
