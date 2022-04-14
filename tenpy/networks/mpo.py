24a6fdefcd947891f24bd364be6988c724118bf0
Sajant Anand <sajantanand@aol.com>
2022-04-13 20:59:31 -0700

    Mismatch charge error.

--- tenpy/networks/mpo.py
+++ tenpy/networks/mpo.py
@@ -2636,11 +2636,11 @@ class MPOTransferMatrix(NpcLinearOperator):
             L = TM.L
             del TM
         init_env_data = {'init_LP': envs[1], 'init_RP': envs[0], 'age_LP': 0, 'age_RP': 0}
-        if first != 0 or last is not None and last % L != L - 1:
+        if first != 0 or (last is not None and last % L != L - 1):
             env = MPOEnvironment(psi, H, psi, **init_env_data)
             if first % L != 0:
                 init_env_data['init_LP'] = env.get_LP(first, store=False)
-            if last % L != L - 1:
+            if last is not None and last % L != L - 1:
                 init_env_data['init_RP'] = env.get_RP(last, store=False)
         if calc_E:
             # We need this for segment excitation energies.
@@ -2649,7 +2649,7 @@ class MPOTransferMatrix(NpcLinearOperator):
                 assert (last + 1) % L == first % L, "Need to have an integer number of unit cells for the bond to be the same."
             SL = psi.get_SL(first)
             if not isinstance(SL, npc.Array):
-                vL, vR = envs[1].get_leg('vR').conj(), envs[0].get_leg('vL').conj()
+                vL, vR = init_env_data['init_LP'].get_leg('vR').conj(), init_env_data['init_RP'].get_leg('vL').conj()
                 SL = npc.diag(SL, vL, dtype=np.promote_types(psi.dtype, H.dtype), labels=['vL', 'vR'])
             E0 = npc.tensordot(init_env_data['init_LP'], SL, axes=(['vR'], ['vL']))
             E0 = npc.tensordot(E0, SL.conj(), axes=(['vR*'], ['vL*']))
