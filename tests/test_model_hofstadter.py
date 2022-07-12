76773d6d77d6b1ee524bcdc440b15d0be1c58dd1
Gunnar Moller <G.Moller@kent.ac.uk>
2022-07-12 19:17:59 +0100

    adjust test_model_hofstadter.py for different conventions for passing hofstadter flux density

--- tests/test_model_hofstadter.py
+++ tests/test_model_hofstadter.py
@@ -9,7 +9,8 @@ def test_HofstadterBosons():
     model_pars = {
         'Lx': 3,
         'Ly': 3,
-        'phi': (1, 3),
+        'flux_p': 1,
+        'flux_q': 3,
         'conserve': 'N',
         'U': 0.456,
         'mu': 0.123,
@@ -22,19 +23,21 @@ def test_HofstadterBosons():
     model_pars['gauge'] = 'symmetric'
     model_pars['Lx'] = model_pars['Ly'] = 2
     model_pars['mx'] = model_pars['my'] = 2
-    model_pars['phi'] = (1, 4)
+    model_pars['flux_p'] = 1
+    model_pars['flux_q'] = 4
     check_general_model(HofstadterBosons, model_pars, {'bc_MPS': ['finite', 'infinite']})
 
 
 @pytest.mark.slow
 def test_HofstadterFermions():
-    model_pars = {'Lx': 3, 'Ly': 3, 'phi': (1, 3), 'conserve': 'N', 'v': 0.456, 'mu': 0.123}
+    model_pars = {'Lx': 3, 'Ly': 3, 'flux_p': 1, 'flux_q': 3, 'conserve': 'N', 'v': 0.456, 'mu': 0.123}
     check_general_model(HofstadterFermions, model_pars, {
         'bc_MPS': ['finite', 'infinite'],
         'gauge': ['landau_x', 'landau_y'],
     })
     model_pars['gauge'] = 'symmetric'
     model_pars['Lx'] = model_pars['Ly'] = 4
-    model_pars['phi'] = (1, 4)
+    model_pars['flux_p'] = 1
+    model_pars['flux_q'] = 4
     model_pars['mx'] = model_pars['my'] = 2
     check_general_model(HofstadterFermions, model_pars, {'bc_MPS': ['finite', 'infinite']})
