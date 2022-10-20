944189fd7d3c93687eadb93873480a8c9229bc66
Jakob Unfried <jakob.unfried@tum.de>
2022-10-20 11:50:32 +0200

    first version of symmetries/spaces

--- tenpy/linalg/symmetries/__init__.py
+++ tenpy/linalg/symmetries/__init__.py
@@ -1,2 +1,3 @@
 from .groups import AbstractSymmetry, FusionStyle, BraidingStyle, no_symmetry, u1_symmetry, z2_symmetry, z3_symmetry, \
     z4_symmetry, su2_symmetry, fermionic_parity
+from .spaces import AbstractSpace, VectorSpace, ProductSpace
---
+++ tenpy/linalg/symmetries/spaces.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+from abc import ABC
+from math import prod
+
+from tenpy.linalg.symmetries.groups import AbstractSymmetry, Sector, no_symmetry
+
+
+class AbstractSpace(ABC):
+    def __init__(self, symmetry: AbstractSymmetry, dim: int):
+        self.symmetry = symmetry
+        self.dim = dim
+
+
+class VectorSpace(AbstractSpace):  # formerly "Leg"
+    def __init__(self, symmetry: AbstractSymmetry, sector_list: list[Sector],
+                 multiplicity_list: list[int] = None, conj: bool = False,
+                 is_real: bool = False):
+        """
+        A vector space, which decomposes into sectors of given symmetry.
+        conj: whether this is the "normal" (i.e. ket) or dual (i.e. bra) space.
+        is_real: whether the space is real or complex
+        """
+        self.sector_list = sector_list
+        if multiplicity_list is None:
+            self.multiplicity_list = [1 for s in sector_list]
+        else:
+            assert len(multiplicity_list) == len(sector_list)
+            self.multiplicity_list = multiplicity_list
+        self.conj = conj
+        dim = sum(symmetry.sector_dim(s) * m for s, m in zip(sector_list, self.multiplicity_list))
+        super().__init__(symmetry=symmetry, dim=dim)
+
+    @classmethod
+    def non_symmetric(cls, dim: int, conj: bool = False, is_real: bool = False):
+        return cls(symmetry=no_symmetry, sector_list=[None], multiplicity_list=[dim], conj=conj, is_real=is_real)
+
+    def __mul__(self, other):
+        if isinstance(other, VectorSpace):
+            return ProductSpace([self, other])
+        if isinstance(other, ProductSpace):
+            return ProductSpace([self, *other.spaces])
+        return NotImplemented
+
+
+class ProductSpace(AbstractSpace):  # formerly "LegPipe"
+    def __init__(self, spaces: list[VectorSpace]):
+        self.spaces = spaces
+        super().__init__(symmetry=spaces[0].symmetry, dim=prod(s.dim for s in spaces))
+
+    def __mul__(self, other):
+        if isinstance(other, VectorSpace):
+            return ProductSpace([*self.spaces, other])
+        if isinstance(other, ProductSpace):
+            return ProductSpace([*self.spaces, *other.spaces])
+        return NotImplemented
