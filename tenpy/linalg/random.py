f0870973ad09878cf8d48f54402662244445d896
Jakob Unfried <jakob.unfried@tum.de>
2023-02-08 15:08:09 +0100

    make linalg.random stub

--- tenpy/linalg/random.py
+++ tenpy/linalg/random.py
@@ -1,27 +1,20 @@
 from __future__ import annotations
 
-from tenpy.linalg.backends import AbstractBackend, Dtype
-from tenpy.linalg.symmetries import VectorSpace
-from tenpy.linalg.tensors import Tensor
+from .backends import AbstractBackend, Dtype
+from .symmetries import VectorSpace
+from .tensors import Tensor
 
 
-# FIXME dont do this
-def uniform(backend: AbstractBackend, legs: list[VectorSpace], labels: list[str] = None, dtype: Dtype = None
-            ) -> Tensor:
-    """
-    Generate a sample from the probability distribution which is uniform on all tensors with the given legs
-    and norm <= 1
-    """
-    return Tensor(backend.random_uniform(legs, dtype), backend, legs=legs, leg_labels=labels, dtype=dtype)
+def uniform() -> Tensor:
+    # TODO is there even a useful and basis-independent notion of a uniform probability distribution?
+    raise NotImplementedError
 
 
-def gaussian(backend: AbstractBackend, legs: list[VectorSpace], sigma: float = 1., labels: list[str] = None,
-             dtype: Dtype = None) -> Tensor:
+def normal(backend: AbstractBackend, *some_args) -> Tensor:
     """
     Generate a sample from the probability distribution p(T) ~ exp(-.5 * (norm(T) / sigma) ** 2)
     """
-    return Tensor(backend.random_gaussian(legs, dtype, sigma), backend, legs=legs, leg_labels=labels, dtype=dtype)
+    raise NotImplementedError  # TODO
 
 
-# TODO Haar unitary, like tenpy OLD
-# TODO random isometry
+# TODO random orthogonal and unitary maps, like in old tenpy
