324aa06edc98f2710c95637df3c83961ad3b8ac4
Jakob Unfried <jakob.unfried@tum.de>
2023-07-11 13:16:26 +0200

    Add new tenpy.linalg.sparse implementation

---
+++ tenpy/linalg/sparse.py
@@ -0,0 +1,217 @@
+"""Providing support for sparse algorithms (using matrix-vector products only).
+
+Some linear algebra algorithms, e.g. Lanczos, do not require the full representations of a linear
+operator, but only the action on a vector, i.e., a matrix-vector product `matvec`. Here we define
+the strucuture of such a general operator, :class:`NpcLinearOperator`, as it is used in our own
+implementations of these algorithms (e.g., :mod:`~tenpy.linalg.lanczos`). Moreover, the
+:class:`FlatLinearOperator` allows to use all the scipy sparse methods by providing functionality
+to convert flat numpy arrays to and from np_conserved arrays.
+
+TODO revise docstring
+"""
+# Copyright 2018-2021 TeNPy Developers, GNU GPLv3
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from numbers import Number
+import warnings
+import numpy as np
+
+from .tensors import AbstractTensor, Shape, inner
+from .backends.abstract_backend import Dtype
+
+
+__all__ = ['TenpyLinearOperator', 'TenpyLinearOperatorWrapper', 'SumTenpyLinearOperator',
+           'ShiftedTenpyLinearOperator', 'ProjectedTenpyLinearOperator']
+
+
+class TenpyLinearOperator(ABC):
+    """Base class for a linear operator acting on tenpy tensors.
+
+    Attributes
+    ----------
+    vector_shape : Shape
+        The shape of tensors that this operator can act on
+    dtype : Dtype
+        The dtype of a full representation of the operator
+    """
+    def __init__(self, vector_shape: Shape, dtype: Dtype):
+        self.vector_shape = vector_shape
+        self.dtype = dtype
+
+    @abstractmethod
+    def matvec(self, vec: AbstractTensor) -> AbstractTensor:
+        """Apply the linear operator to a "vector".
+
+        We consider as vectors all tensors of the shape given by :attr:`vector_shape`,
+        and in particular allow multi-leg tensors as "vectors".
+        The result of `matvec` must be a tensor of the same shape.
+        """
+        ...
+
+    @abstractmethod
+    def to_matrix(self):
+        # TODO
+        #   - should the result be a "matrix", i.e. forced to have two legs?
+        #     if not, rename the method?
+        #   - implement in derived classes
+        #   - add tests
+        raise NotImplementedError
+
+    def adjoint(self) -> TenpyLinearOperator:
+        """Return the hermitian conjugate operator.
+
+        If `self` is hermitian, subclasses *can* choose to implement this to define
+        the adjoint operator of `self` to be `self`.
+        """
+        raise NotImplementedError("No adjoint defined")
+
+
+class TenpyLinearOperatorWrapper(TenpyLinearOperator, ABC):
+    """Base class for wrapping around another :class:`TenpyLinearOperator`.
+
+    Attributes which are not explicitly set, e.g. via `self.attribute = value` or by
+    defining methods default to the attributes of the `original_operator`.
+
+    This behavior is particularly useful when wrapping some concrete subclass of TenpyLinearOperator,
+    which defines additional attributes.
+    Using this base class, we can define the wrappers below without considering those extra attributes.
+
+    .. warning ::
+        If there are multiple levels of wrapping operators, the order might be critical to get
+        correct results; e.g. :class:`OrthogonalTenpyLinearOperator` needs to be the outer-most
+        wrapper to produce correct results and/or be efficient.
+
+    Parameters
+    ----------
+    original_operator : :class:`TenpyLinearOperator`
+        The original operator implementing the `matvec`.
+    """
+    def __init__(self, original_operator: TenpyLinearOperator):
+        self.original_operator = original_operator
+        # TODO (JU) should we call TenpyLinearOperator.__init__ or super().__init__ here?
+        #      Its current implementation only sets attributes, which we dont need because
+        #      we hack into __getattr__
+
+    def __getattr__(self, name):
+        # note: __getattr__ (unlike __getattribute__) is only called if the attribute is not
+        #       found in the __dict__, so it is the fallback for attributes that are not explicitly set.
+        return getattr(self.original_operator, name)
+
+    def unwrapped(self, recursive: bool = True) -> TenpyLinearOperator:
+        """Return the original `TenpyLinearOperator`
+
+        By default, unwrapping is done recursively, such that the result is *not* a `TenpyLinearOperatorWrapper`.
+        """
+        parent = self.original_operator
+        if not recursive:
+            return parent
+        for _ in range(10000):
+            try:
+                parent = parent.unwrapped()
+            except AttributeError:
+                # parent has no :meth:`unwrapped`, so we can stop unwrapping
+                return parent
+        raise ValueError('maximum recursion depth for unwrapping reached')
+
+
+class SumTenpyLinearOperator(TenpyLinearOperatorWrapper):
+    """The sum of multiple operators"""
+    def __init__(self, original_operator: TenpyLinearOperator, *more_operators: TenpyLinearOperator):
+        super().__init__(original_operator=original_operator)
+        assert all(op.vector_shape == original_operator.vector_shape for op in more_operators)
+        self.more_operators = more_operators
+        self.dtype = Dtype.common(original_operator.dtype, *(op.dtype for op in more_operators))
+
+    def matvec(self, vec: AbstractTensor) -> AbstractTensor:
+        return sum((op.matvec(vec) for op in self.more_operators), self.original_operator.matvec(vec))
+
+    def adjoint(self) -> TenpyLinearOperator:
+        return SumTenpyLinearOperator(self.original_operator.adjoint(),
+                                      *(op.adjoint() for op in self.more_operators))
+
+
+class ShiftedTenpyLinearOperator(TenpyLinearOperatorWrapper):
+    """A shifted operator, i.e. ``original_operator + shift * identity``.
+
+    This can be useful e.g. for better Lanczos convergence.
+    """
+    def __init__(self, original_operator: TenpyLinearOperator, shift: Number):
+        if shift in [0, 0.]:
+            warnings.warn('shift=0: no need for ShiftedTenpyLinearOperator', stacklevel=2)
+        super().__init__(original_operator=original_operator)
+        self.shift = shift
+        if np.iscomplexobj(shift):
+            self.dtype = original_operator.dtype.to_complex
+
+    def matvec(self, vec: AbstractTensor) -> AbstractTensor:
+        return self.original_operator.matvec(vec) + self.shift * vec
+
+    def adjoint(self):
+        return ShiftedTenpyLinearOperator(original_operator=self.original_operator.adjoint(),
+                                          shift=np.conj(self.shift))
+
+
+class ProjectedTenpyLinearOperator(TenpyLinearOperatorWrapper):
+    """Projected version ``P H P + penalty * (1 - P)`` of an original operator ``H``.
+
+    The projector ``P = 1 - sum_o |o> <o|`` is given in terms of a set :attr:`ortho_vecs` of vectors
+    ``|o>``.
+    
+    The result is that all vectors from the subspace spanned by the :attr:`ortho_vecs` are eigenvectors
+    with eigenvalue `penalty`, while the eigensystem in the "rest" (i.e. in the orthogonal complement
+    to that subspace) remains unchanged.
+    This can be used to exclude the :attr:`ortho_vecs` from extremal eigensolvers.
+
+    Parameters
+    ----------
+    original_operator : :class:`TenpyLinearOperator`
+        The original operator, denoted ``H`` in the summary above.
+    ortho_vecs : list of :class:`~tenpy.linalg.tensors.AbstractTensor`
+        TODO (JU) different name?
+        The list of vectors spanning the projected space.
+        They need not be orthonormal, as Gram-Schmidt is performed on them explicitly.
+    penalty : complex, optional
+        See summary above. Defaults to ``None``, which is equivalent to ``0.``.
+    """
+    def __init__(self, original_operator: TenpyLinearOperator, ortho_vecs: list[AbstractTensor],
+                 penalty: Number = None):
+        if len(ortho_vecs) == 0:
+            warnings.warn('empty ortho_vecs: no need for ProjectedTenpyLinearOperator', stacklevel=2)
+        super().__init__(original_operator=original_operator)
+        assert all(v.shape == original_operator.vector_shape for v in ortho_vecs)
+        from .lanczos import gram_schmidt
+        self.ortho_vecs = gram_schmidt(ortho_vecs)
+        self.penalty = penalty
+
+    def matvec(self, vec: AbstractTensor) -> AbstractTensor:
+        res = vec
+        # form ``P vec`` and keep coefficients for later use in the penalty term
+        coefficients = []
+        for o in self.ortho_vecs:
+            c = inner(o, res)
+            coefficients.append(c)
+            res = res - c * o
+        # ``H P vec``
+        res = self.original_operator.matvec(res)
+        # ``P H P vec``
+        for o in reversed(self.ortho_vecs):
+            # reverse: more obviously Hermitian.
+            # TODO (JU) i dont see how the order makes a difference here or why reverse is better...
+            #           @jhauschild, i just took this from the previous implementation.
+            #           could you expand the explanation?
+            res = res - inner(o, res) * o
+        if self.penalty is not None:
+            for c, o in zip(coefficients, self.ortho_vecs):
+                res = res + self.penalty * c * o
+        return res
+        
+    def adjoint(self) -> TenpyLinearOperator:
+        return ProjectedTenpyLinearOperator(
+            original_operator=self.original_operator.adjoint(),
+            ortho_vecs=self.ortho_vecs,  # hc(|o> <o|) = |o> <o|  ->  can use same ortho_vecs
+            penalty=None if self.penalty is None else np.conj(self.penalty)
+        )
+
+
+# TODO (JU) port FlatLinearOperator from old
---
+++ tests/linalg/test_sparse.py
@@ -0,0 +1,202 @@
+"""A collection of tests for tenpy.linalg.sparse"""
+# Copyright 2023-2023 TeNPy Developers, GNU GPLv3
+import numpy as np
+import numpy.testing as npt
+import pytest
+import warnings
+
+from tenpy.linalg import sparse
+from tenpy.linalg.tensors import AbstractTensor, Tensor, Shape, Dtype, almost_equal
+
+
+# define a few simple operators to test the wrappers:
+
+
+class ConstantDummyOperator(sparse.TenpyLinearOperator):
+    def __init__(self, value):
+        super().__init__(vector_shape=value.shape, dtype=Dtype.complex128)
+        self.value = value
+        self.some_weird_attribute = 'arbitrary value'
+
+    def some_unrelated_function(self, x):
+        return 2 * x
+
+    def matvec(self, vec: AbstractTensor) -> AbstractTensor:
+        return self.value
+
+    def adjoint(self):
+        return ConstantDummyOperator(np.conj(self.value), self.vector_shape)
+
+
+class TensorDummyOperator(sparse.TenpyLinearOperator):
+    def __init__(self, tensor: Tensor):
+        assert tensor.labels == ['a', 'b*', 'a*', 'b']
+        acts_on = ['a', 'b']
+        vector_shape = Shape(legs=tensor.get_legs(acts_on), labels=acts_on)
+        super().__init__(vector_shape=vector_shape, dtype=tensor.dtype)
+        self.tensor = tensor
+        self.some_weird_attribute = 42
+
+    def some_unrelated_function(self, x):
+        return 'buzz'
+
+    def matvec(self, vec: AbstractTensor) -> AbstractTensor:
+        return self.tensor.tdot(vec, ['a*', 'b*'], ['a', 'b'])
+
+    def adjoint(self):
+        return TensorDummyOperator(self.tensor.conj())
+
+
+def test_SumTenpyLinearOperator(tensor_rng, vector_space_rng):
+    a = vector_space_rng()
+    b = vector_space_rng()
+    T = tensor_rng(legs=[a, b.dual, a.dual, b], real=False, labels=['a', 'b*', 'a*', 'b'])
+    vec1 = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    vec3 = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    vec = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    
+    op1 = ConstantDummyOperator(vec1)
+    op2 = TensorDummyOperator(T)
+    op3 = ConstantDummyOperator(vec3)
+
+    print('single operator')
+    op = sparse.SumTenpyLinearOperator(op1)
+    # check matvec correct
+    assert almost_equal(op.matvec(vec), vec1)
+    # check access to attributes of original_operator
+    assert op.some_weird_attribute == 'arbitrary value'
+    assert op.some_unrelated_function(2) == 4
+
+    print('two operators')
+    print(op1.vector_shape)
+    print(op2.vector_shape)
+    op = sparse.SumTenpyLinearOperator(op2, op1)
+    assert almost_equal(op.matvec(vec), vec1 + T.tdot(vec, ['a*', 'b*'], ['a', 'b']))
+    assert op.some_weird_attribute == 42
+    assert op.some_unrelated_function(2) == 'buzz'
+
+    print('three operators')
+    op = sparse.SumTenpyLinearOperator(op1, op2, op3)
+    assert almost_equal(op.matvec(vec), vec1 + vec3 + T.tdot(vec, ['a*', 'b*'], ['a', 'b']))
+    assert op.some_weird_attribute == 'arbitrary value'
+    assert op.some_unrelated_function(2) == 4
+
+
+def test_ShiftedTenpyLinearOperator(tensor_rng, vector_space_rng):
+    a = vector_space_rng()
+    b = vector_space_rng()
+    vec = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    vec1 = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    op1 = ConstantDummyOperator(vec1)
+    shift = 5.j
+    
+    op = sparse.ShiftedTenpyLinearOperator(op1, shift)
+    assert almost_equal(op.matvec(vec), vec1 + shift * vec)
+    assert op.some_weird_attribute == 'arbitrary value'
+    assert op.some_unrelated_function(2) == 4
+
+
+@pytest.mark.parametrize('penalty', [None, 2.-.3j])
+def test_ProjectedTenpyLinearOperator(tensor_rng, vector_space_rng, penalty):
+    a = vector_space_rng()
+    b = vector_space_rng()
+    o1 = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    o2 = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    vec1 = tensor_rng(legs=[a, b], labels=['a', 'b'])
+    op1 = ConstantDummyOperator(vec1)
+
+    pytest.xfail('Need to port gram_schmidt first')  # TODO
+    op = sparse.ProjectedTenpyLinearOperator(op1, [o1, o2], penalty=penalty)
+
+    print('check vector in ortho_vecs subspace')
+    expect = 0. * o1 if penalty is None else penalty * o1
+    assert almost_equal(op.matvec(o1), expect)
+
+    print('check vector orthogonal to ortho_vecs')
+    vec = vec1 - o1.inner(vec1) * o1 - o2.inner(vec) * o2
+    assert almost_equal(op.matvec(vec), op1.matvec(vec))
+
+    assert op.some_weird_attribute == 'arbitrary value'
+    assert op.some_unrelated_function(2) == 4
+
+
+def test_FlatLinearOperator(n=30, k=5, tol=1.e-14):
+    pytest.xfail('Need to port the implementation first')
+    # TODO blindly pasted old code below:
+    # leg = gen_random_legcharge(ch, n)
+    # H = npc.Array.from_func_square(rmat.GUE, leg)
+    # H_flat = H.to_ndarray()
+    # E_flat, psi_flat = np.linalg.eigh(H_flat)
+    # E0_flat, psi0_flat = E_flat[0], psi_flat[:, 0]
+    # qtotal = npc.detect_qtotal(psi0_flat, [leg])
+
+    # H_sparse = OLD_sparse.FlatLinearOperator.from_NpcArray(H, charge_sector=qtotal)
+    # psi_init = npc.Array.from_func(np.random.random, [leg], qtotal=qtotal)
+    # psi_init /= npc.norm(psi_init)
+    # psi_init_flat = H_sparse.npc_to_flat(psi_init)
+
+    # # check diagonalization
+    # E, psi = scipy.sparse.linalg.eigsh(H_sparse, k, v0=psi_init_flat, which='SA')
+    # E0, psi0 = E[0], psi[:, 0]
+    # print("full spectrum:", E_flat)
+    # print("E0 = {E0:.14f} vs exact {E0_flat:.14f}".format(E0=E0, E0_flat=E0_flat))
+    # print("|E0-E0_flat| / |E0_flat| =", abs((E0 - E0_flat) / E0_flat))
+    # assert (abs((E0 - E0_flat) / E0_flat) < tol)
+    # psi0_H_psi0 = np.inner(psi0.conj(), H_sparse.matvec(psi0)).item()
+    # print("<psi0|H|psi0> / E0 = 1. + ", psi0_H_psi0 / E0 - 1.)
+    # assert (abs(psi0_H_psi0 / E0 - 1.) < tol)
+
+
+def test_FlatHermitianOperator(n=30, k=5, tol=1.e-14):
+    pytest.xfail('Need to port the implementation first')
+    # TODO blindly pasted old code below:
+    # leg = gen_random_legcharge(ch, n // 2)
+    # leg2 = gen_random_legcharge(ch, 2)
+    # pipe = npc.LegPipe([leg, leg2], qconj=+1)
+    # H = npc.Array.from_func_square(rmat.GUE, pipe, labels=["(a.b)", "(a*.b*)"])
+    # H_flat = H.to_ndarray()
+    # E_flat, psi_flat = np.linalg.eigh(H_flat)
+    # E0_flat, psi0_flat = E_flat[0], psi_flat[:, 0]
+    # qtotal = npc.detect_qtotal(psi0_flat, [pipe])
+
+    # H_sparse = OLD_sparse.FlatHermitianOperator.from_NpcArray(H, charge_sector=qtotal)
+    # psi_init = npc.Array.from_func(np.random.random, [pipe], qtotal=qtotal, labels=["(a.b)"])
+    # psi_init /= npc.norm(psi_init)
+    # psi_init_flat = H_sparse.npc_to_flat(psi_init)
+
+    # # check diagonalization
+    # E, psi = scipy.sparse.linalg.eigsh(H_sparse, k, v0=psi_init_flat, which='SA')
+    # E0, psi0 = E[0], psi[:, 0]
+    # print("full spectrum:", E_flat)
+    # print("E0 = {E0:.14f} vs exact {E0_flat:.14f}".format(E0=E0, E0_flat=E0_flat))
+    # print("|E0-E0_flat| / |E0_flat| =", abs((E0 - E0_flat) / E0_flat))
+    # assert (abs((E0 - E0_flat) / E0_flat) < tol)
+    # psi0_H_psi0 = np.inner(psi0.conj(), H_sparse.matvec(psi0)).item()
+    # print("<psi0|H|psi0> / E0 = 1. + ", psi0_H_psi0 / E0 - 1.)
+    # assert (abs(psi0_H_psi0 / E0 - 1.) < tol)
+
+    # # split H to check `FlatHermitianOperator.from_guess_with_pipe`.
+    # print("=========")
+    # print("split legs and define separate matvec")
+    # assert psi_init.legs[0] is pipe
+    # psi_init_split = psi_init.split_legs([0])
+    # H_split = H.split_legs()
+
+    # def H_split_matvec(vec):
+    #     vec = npc.tensordot(H_split, vec, [["a*", "b*"], ["a", "b"]])
+    #     # TODO as additional challenge, transpose the resulting vector
+    #     return vec
+
+    # H_sparse_split, psi_init_split_flat = OLD_sparse.FlatLinearOperator.from_guess_with_pipe(
+    #     H_split_matvec, psi_init_split, dtype=H_split.dtype)
+
+    # # diagonalize
+    # E, psi = scipy.sparse.linalg.eigsh(H_sparse_split, k, v0=psi_init_split_flat, which='SA')
+    # E0, psi0 = E[0], psi[:, 0]
+    # print("full spectrum:", E_flat)
+    # print("E0 = {E0:.14f} vs exact {E0_flat:.14f}".format(E0=E0, E0_flat=E0_flat))
+    # print("|E0-E0_flat| / |E0_flat| =", abs((E0 - E0_flat) / E0_flat))
+    # assert (abs((E0 - E0_flat) / E0_flat) < tol)
+    # psi0_H_psi0 = np.inner(psi0.conj(), H_sparse.matvec(psi0)).item()
+    # print("<psi0|H|psi0> / E0 = 1. + ", psi0_H_psi0 / E0 - 1.)
+    # assert (abs(psi0_H_psi0 / E0 - 1.) < tol)
