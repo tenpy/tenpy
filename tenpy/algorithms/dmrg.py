tenpy/tenpy

commit 90a76a09052ea51012eeca1cde35a9372ffe1b6e

update sweep schedule to include an extra update on site 0 at the end of the sweep
This leaves the MPS in form A S B B ... B after the sweep,
instead of B A S B B ... B, where there is an inverse of S required for get_theta(0, 2)
(Instead, get_theta(L-1, 2) requires the inverse of S)
At the moment, this implies one additional update for each sweep!

tenpy/algorithms/dmrg.py
@@ -1523,7 +1523,14 @@ class TwoSiteDMRGEngine(DMRGEngine):
                 U.ireplace_label('vL', '(vL.p0)')
                 VH.ireplace_label('(p0.vR)', '(p1.vR)')
             else:
-                assert False
+                # special case at end of sweep where we don't expand
+                # same as no mixer
+                qtotal_i0 = self.env.bra.get_B(i0, form=None).qtotal
+                U, S, VH, err, _ = svd_theta(theta,
+                                            self.trunc_params,
+                                            qtotal_LR=[qtotal_i0, None],
+                                            inner_labels=['vR', 'vL'])
+                S_a = S
         else:
             assert False, "mixer acting on wired number of sites"
         U.ireplace_label('(vL.p0)', '(vL.p)')

tenpy/algorithms/mps_common.py
@@ -419,20 +419,20 @@ class Sweep(Algorithm):
         n = self.EffectiveH.length
         if self.finite:
             assert L > n
-            i0s = list(range(0, L - n)) + list(range(L - n, 0, -1))
-            move_right = [True] * (L - n) + [False] * (L - n)
-            update_LP_RP = [[True, False]] * (L - n) + [[False, True]] * (L - n)
+            i0s = list(range(0, L - n)) + list(range(L - n, -1, -1))
+            move_right = [True] * (L - n) + [False] * (L - n + 1)  + [True]
+            update_LP_RP = [[True, False]] * (L - n) + [[False, True]] * (L - n - 1) + [[False, False]]
         elif n == 2:
             assert L >= 2
-            i0s = list(range(0, L)) + list(range(L, 0, -1))
-            move_right = [True] * L + [False] * L
+            i0s = list(range(0, L)) + list(range(L, -1, -1))
+            move_right = [True] * L + [False] * L + [True]
             update_LP_RP = [[True, True]] * 2 + [[True, False]] * (L-2) + \
-                           [[True, True]] * 2 + [[False, True]] * (L-2)
+                           [[True, True]] * 2 + [[False, True]] * (L-2) + [[False, False]]
         elif n == 1:
-            i0s = list(range(0, L)) + list(range(L, 0, -1))
-            move_right = [True] * L + [False] * L
+            i0s = list(range(0, L)) + list(range(L, -1, -1))
+            move_right = [True] * L + [False] * L + [True]
             update_LP_RP = [[True, True]] + [[True, False]] * (L-1) + \
-                           [[True, True]] + [[False, True]] * (L-1)
+                           [[True, True]] + [[False, True]] * (L-1) + [[False, False]]
         else:
             assert False, "n_optimize is neither 1 nor 2!?"
         return zip(i0s, move_right, update_LP_RP)

tenpy/algorithms/tdvp.py
@@ -164,9 +164,10 @@ class TwoSiteTDVPEngine(TDVPEngine):
         self.trunc_err = TruncationError()
 
     def get_sweep_schedule(self):
-        """Slightly different sweep schedule than DMRG"""
         L = self.psi.L
         if self.finite:
+            # might be the same as Sweep.get_sweep_schedule, but override in case upsteam changes:
+            # the `dt` chosen below needs to fit to the sweep schedule to ensure correct evolution
             i0s = list(range(0, L - 2)) + list(range(L - 2, -1, -1))
             move_right = [True] * (L - 2) + [False] * (L - 1)
             update_LP_RP = [[True, False]] * (L - 2) + [[False, True]] * (L - 2) + [[False, False]]
@@ -260,9 +261,10 @@ class SingleSiteTDVPEngine(TDVPEngine):
     EffectiveH = OneSiteH
 
     def get_sweep_schedule(self):
-        """slightly different sweep schedule than DMRG"""
         L = self.psi.L
         if self.finite:
+            # might be the same as Sweep.get_sweep_schedule, but override in case upsteam changes:
+            # the `dt` chosen below needs to fit to the sweep schedule to ensure correct evolution
             i0s = list(range(0, L - 1)) + list(range(L - 1, -1, -1))
             move_right = [True] * (L - 1) + [False] * L
             update_LP_RP = [[True, False]] * (L - 1) + [[False, True]] * (L - 1) + [[False, False]]

tenpy/networks/mps.py
@@ -2035,7 +2035,7 @@ class MPS(BaseMPSExpectationValue):
         if new_form is not None and old_form != new_form:
             if old_form is None:
                 raise ValueError("can't convert form of non-canonical state!")
-            # try avoid inverses of singular values, see :issue:`???` # TODO
+            # try avoid inverses of singular values, see :issue:`292`
             # typical case e.g. in iDMRG when inserting unit cells
             if avoid_S_inverse and old_form == (1., 0.) and new_form == (0., 1.):
                 # got A, need B

commit bdf3e10333a79bd186866e96303b2391edccd18c

allow move_right=None in the DMRG schedule
also fixes bugs in the sweep schedule introduced in 90a76a09

tenpy/algorithms/dmrg.py
@@ -41,7 +41,7 @@ logger = logging.getLogger(__name__)
 from ..linalg import np_conserved as npc
 from ..networks.mps import MPSEnvironment
 from ..linalg.krylov_based import lanczos_arpack, LanczosGroundState
-from .truncation import truncate, svd_theta
+from .truncation import truncate, svd_theta, TruncationError
 from ..tools.params import asConfig
 from ..tools.math import entropy
 from ..tools.process import memory_usage
@@ -645,7 +645,8 @@ class DMRGEngine(Sweep):
             E0, N, ov_change = None, 0, 0.
         theta = self.prepare_svd(theta)
         U, S, VH, err, S_approx = self.mixed_svd(theta)
-        self._entropy_approx[(i0 + n_opt - 1) % self.psi.L] = entropy(S_approx**2)
+        if S_approx is not None:
+            self._entropy_approx[(i0 + n_opt - 1) % self.psi.L] = entropy(S_approx**2)
         self.set_B(U, S, VH)
         update_data = {
             'E0': E0,
@@ -672,7 +673,7 @@ class DMRGEngine(Sweep):
         i0 = self.i0
         E_trunc = None
         if self._meas_E_trunc or E0 is None:
-            i = i0 if self.n_optimize == 2 or self.move_right else i0 - 1
+            i = i0 if self.n_optimize == 2 or self.move_right is not False else i0 - 1
             E_trunc = self.env.full_contraction(i).real  # uses updated LP/RP (if calculated)
             if E0 is None:
                 E0 = E_trunc
@@ -1115,12 +1116,12 @@ class SingleSiteDMRGEngine(DMRGEngine):
         if self.combine:
             if self.move_right:
                 theta.itranspose(['(vL.p0)', 'vR'])  # ensure the order.
-            else:
+            else:  # left move or no move
                 theta.itranspose(['vL', '(p0.vR)'])  # ensure the order.
         else:
             if self.move_right:
                 theta = theta.combine_legs(['vL', 'p0'], qconj=+1, new_axes=0)
-            else:
+            else:  # left move or no move
                 theta = theta.combine_legs(['p0', 'vR'], qconj=-1, new_axes=1)
         return theta
 
@@ -1129,12 +1130,12 @@ class SingleSiteDMRGEngine(DMRGEngine):
 
         The goal is to split theta and truncate it. For a move to the right::
 
-            |             -- theta -- next_B --   ==>    -- U -- S -- VH --
+            |             -- theta -- next_B --   ==>    -- U -- S == VH --
             |                  |        |                   |         |
 
         For a move to the left::
 
-            |   -- next_A -- theta --   ==>    -- U -- S -- VH --
+            |   -- next_A -- theta --   ==>    -- U -- S == VH --
             |        |         |                  |         |
 
         Note that `theta` lives on the same site :attr:`i0` in both cases,
@@ -1144,7 +1145,9 @@ class SingleSiteDMRGEngine(DMRGEngine):
         Without a mixer, this is done by a simple svd and truncation of Schmidt values of theta
         followed by the absorption of `VH` into `next_B` (`U` into `next_A`).
 
-        With a mixer, the state/density matrix is perturbed before the SVD.
+        With a mixer, the state/density matrix is perturbed before the SVD, in particular to
+        increase the bond dimension on the bold legs above (or even both legs around S, if both
+        `update_LP` and `update_RP` are true).
         The details of the perturbation are defined by the :class:`Mixer` class.
 
         Parameters
@@ -1171,18 +1174,24 @@ class SingleSiteDMRGEngine(DMRGEngine):
         mixer = self.mixer
         move_right = self.move_right
         update_LP, update_RP = self.update_LP_RP
-        if self.move_right:
+        if self.move_right is True:
             next_B = self.psi.get_B(self.i0 + 1, form='B')
             next_B = next_B.combine_legs(['p', 'vR'], qconj=-1, new_axes=1)
             if update_RP:
                 # make sure that `next_B` is in right-canonical form
                 assert self.psi.form[(self.i0 + 1) % self.psi.L] == (0., 1.)
-        else:
+        elif self.move_right is False:
             next_A = self.psi.get_B(self.i0 - 1, form='A')
             next_A = next_A.combine_legs(['vL', 'p'], qconj=1, new_axes=0)
             if update_LP:
                 # make sure that `next_A` is in left-canonical form
                 assert self.psi.form[(self.i0 - 1) % self.psi.L] == (1., 0.)
+        elif self.move_right is None:
+            # special case when not moving at all.
+            return None, theta.ireplace_label('(p0.vR)', '(p.vR)'), None, TruncationError(), None
+        else:
+            assert False, "invalid move_right"
+        # assert self.move_right is not None, "not-moving case handled above"
 
         if mixer is None:
             qtotal = [theta.qtotal, None] if move_right else [None, theta.qtotal]
@@ -1226,7 +1235,7 @@ class SingleSiteDMRGEngine(DMRGEngine):
                 U = next_A
                 VH.ireplace_label('(p0.vR)', '(p.vR)')
         else:
-            # just use two-site theta
+            # fallback: just use two-site theta for mixer decomposition
             if self.move_right:
                 next_B.ireplace_label('(p.vR)', '(p1.vR)')
                 theta = npc.tensordot(theta, next_B, axes=['vR', 'vL'])
@@ -1257,6 +1266,12 @@ class SingleSiteDMRGEngine(DMRGEngine):
             The middle part returned by the SVD, ``theta = U S VH``.
             Without a mixer just the singular values, with enabled `mixer` a 2D array.
         """
+        if self.move_right is None:
+            # special case if not moving at all
+            assert U is None and VH is None
+            theta = S.split_legs(['(p.vR)'])
+            self.psi.set_B(self.i0, theta, 'Th')
+            return
         i_L, i_R = self._update_env_inds()  # left and right updated sites
         A0 = U.split_legs(['(vL.p)'])
         B1 = VH.split_legs(['(p.vR)'])

tenpy/algorithms/mps_common.py
@@ -98,7 +98,7 @@ class Sweep(Algorithm):
         Only set during sweep.
         Left-most of the `EffectiveH.length` sites to be updated in :meth:`update_local`.
     move_right : bool | None
-        Only set during sweep.
+        Only set during sweep, see :meth:`get_sweep_schedule`.
         Whether the next `i0` of the sweep will be right (`True`), left (`False`) or at the same
         position (`None`) as the current one.
     update_LP_RP : (bool, bool)
@@ -126,7 +126,7 @@ class Sweep(Algorithm):
     """
     DefaultMixer = None
     use_mixer_by_default = False  # The default for the "mixer" config option
-    
+
     def __init__(self, psi, model, options, *, orthogonal_to=None, **kwargs):
         if not hasattr(self, "EffectiveH"):
             raise NotImplementedError("Subclass needs to set EffectiveH")
@@ -142,8 +142,8 @@ class Sweep(Algorithm):
         self.ortho_to_envs = []
         self.init_env(model, resume_data=self.resume_data, orthogonal_to=orthogonal_to)
         self.i0 = 0
-        self.move_right = True
-        self.update_LP_RP = (True, False)
+        self.move_right = None
+        self.update_LP_RP = (False, False)
 
     @property
     def engine_params(self):
@@ -429,7 +429,7 @@ class Sweep(Algorithm):
                     self.mixer_deactivate()
                 else:
                     self.mixer = mixer
-            
+
         return np.max(self.trunc_err_list)
 
     def get_sweep_schedule(self):
@@ -455,21 +455,22 @@ class Sweep(Algorithm):
         if self.finite:
             assert L > n
             i0s = list(range(0, L - n)) + list(range(L - n, -1, -1))
-            move_right = [True] * (L - n) + [False] * (L - n + 1)  + [True]
-            update_LP_RP = [[True, False]] * (L - n) + [[False, True]] * (L - n - 1) + [[False, False]]
+            move_right = [True] * (L - n) + [False] * (L - n)  + [None]
+            update_LP_RP = [[True, False]] * (L - n) + [[False, True]] * (L - n) + [[False, False]]
         elif n == 2:
             assert L >= 2
             i0s = list(range(0, L)) + list(range(L, -1, -1))
-            move_right = [True] * L + [False] * L + [True]
+            move_right = [True] * L + [False] * L + [None]
             update_LP_RP = [[True, True]] * 2 + [[True, False]] * (L-2) + \
                            [[True, True]] * 2 + [[False, True]] * (L-2) + [[False, False]]
         elif n == 1:
             i0s = list(range(0, L)) + list(range(L, -1, -1))
-            move_right = [True] * L + [False] * L + [True]
+            move_right = [True] * L + [False] * L + [None]
             update_LP_RP = [[True, True]] + [[True, False]] * (L-1) + \
                            [[True, True]] + [[False, True]] * (L-1) + [[False, False]]
         else:
             assert False, "n_optimize is neither 1 nor 2!?"
+        # TODO: check whether VariationalCompression.get_sweep_schedule() is compatible/necessary?
         return zip(i0s, move_right, update_LP_RP)
 
     def _cache_optimize(self):
@@ -481,14 +482,16 @@ class Sweep(Algorithm):
                 'short_term_LP': [i0, i0 + 1],
                 'short_term_RP': [i0, i0 + 1],
             }
-            if move_right:
+            if move_right is True:
                 kwargs['preload_RP'] = i0 + 2
             elif move_right is None:
                 pass  # not moving. nothing to preload
-            else:
+            elif move_right is False:
                 kwargs['preload_LP'] = i0 - 1
+            else:
+                assert False, f"invalid move_right = {move_right!r}"
         elif self.n_optimize == 1:
-            if move_right:
+            if move_right is True:
                 kwargs = {
                     'short_term_LP': [i0, i0 + 1],
                     'short_term_RP': [i0],
@@ -499,12 +502,14 @@ class Sweep(Algorithm):
                     'short_term_LP': [i0],
                     'short_term_RP': [i0],
                 }
-            else:
+            elif move_right is False:
                 kwargs = {
                     'short_term_LP': [i0],
                     'short_term_RP': [i0 - 1, i0],
                     'preload_LP': i0 - 1,
                 }
+            else:
+                assert False, f"invalid move_right = {move_right!r}"
         else:
             raise ValueError(f"unexpected `n_optimize` = {self.n_optimize!r}")
         for env in self._all_envs:
@@ -559,11 +564,14 @@ class Sweep(Algorithm):
         For two-site algorithms with :attr:`n_optimize` = 2, this always optimizes the
         sites :attr:`i0` and `i0` + 1.
         For single-site algorithms, the effective H only acts on site `i0`, but afterwards it
-        also updates the bond to the *right* if :attr:`move_right` is True,
-        or the bond to the left if :attr:`move_right` is False.
+        might also need to updates the bond to the *right* if :attr:`move_right` is True,
+        or the bond to the *left* if :attr:`move_right` is False,
+        (or just site `i0` if :attr:`move_right` is None).
         Since the svd for truncation gives tensors to be multiplied into the tensors on both sides
         of the bond, tensors of two sites are updated even for single-site algorithms:
         when right-moving, site `i0` + 1 is also updated; site `i0` - 1 when left-moving.
+        When ``move_right is None``, the `update_local` should update site only site `i0`,
+        often to `Th` form.
 
         Parameters
         ----------
@@ -588,13 +596,15 @@ class Sweep(Algorithm):
             Whatever is returned by :meth:`update_local`.
         """
         i_L, i_R = self._update_env_inds()  # left and right updated sites
-        all_envs = self._all_envs
-        for env in all_envs:
-            # clean up the updated center bond
-            env.del_LP(i_R)
-            env.del_RP(i_L)
-        # possibly recalculated updated center bonds
         update_LP, update_RP = self.update_LP_RP
+        if self.move_right is not None:
+            for env in self._all_envs:
+                # clean up the updated center bond
+                env.del_LP(i_R)
+                env.del_RP(i_L)
+        else:
+            assert not update_LP and not update_RP, "update LP/RP when not moving"
+        # possibly recalculate updated center bonds
         if update_LP:
             self.eff_H.update_LP(self.env, i_R, update_data['U'])  # possibly optimized
             for env in self.ortho_to_envs:
@@ -607,13 +617,20 @@ class Sweep(Algorithm):
     def _update_env_inds(self):
         n = self.n_optimize  # = 1 or 2
         move_right = self.move_right
-        if n == 2 or move_right:
+        if n == 2:
             i_L = self.i0
             i_R = self.i0 + 1
-        else:  # n == 1 and left moving
-            # TODO is this also correct if move_right is None?
-            i_L = self.i0 - 1
-            i_R = self.i0
+        else:
+            if move_right is True:
+                i_L = self.i0
+                i_R = self.i0 + 1
+            elif move_right is False:
+                i_L = self.i0 - 1
+                i_R = self.i0
+            elif move_right is None:
+                i_L = self.i0 - 1
+                i_R = self.i0 + 1
+            else: assert False, "invalid move_right"
         return i_L, i_R
 
     def post_update_local(self, err, **update_data):
@@ -630,6 +647,8 @@ class Sweep(Algorithm):
         For large MPO bond dimensions, these environments are by far the biggest part in memory,
         so this is a valuable optimiztion to reduce memory requirements.
         """
+        if self.move_right is None:
+            return  # no movement: still need the envs!
         i_L, i_R = self._update_env_inds()  # left and right updated site
         # envs between `i_L` and `i_R` where already deleted and updated in `update_env`
         i0 = self.i0
@@ -747,7 +766,7 @@ class EffectiveH(NpcLinearOperator):
         calculating charge combinations in the contractions.
     move_right : bool | None, optional
         Whether the sweeping algorithm that calls for an `EffectiveH` is moving to the right,
-        to the left or not moving.
+        to the left or not moving. See also :attr:`Sweep.move_right`.
 
     Attributes
     ----------
@@ -769,7 +788,7 @@ class EffectiveH(NpcLinearOperator):
     length = None
     acts_on = None
 
-    def __init__(self, env, i0, combine=False, move_right=True):
+    def __init__(self, env, i0, combine=False, move_right=None):
         raise NotImplementedError("This function should be implemented in derived classes")
 
     def combine_theta(self, theta):
@@ -871,7 +890,7 @@ class OneSiteH(EffectiveH):
     length = 1
     acts_on = ['vL', 'p0', 'vR']
 
-    def __init__(self, env, i0, combine=False, move_right=True):
+    def __init__(self, env, i0, combine=False, move_right=None):
         self.i0 = i0
         self.LP = env.get_LP(i0)
         self.RP = env.get_RP(i0)
@@ -905,7 +924,7 @@ class OneSiteH(EffectiveH):
                 # '(vR*.p0)', 'wR', 'vR'
                 theta = npc.tensordot(theta, self.RP, axes=[['wR', 'vR'], ['wL', 'vL']])
                 theta.ireplace_labels(['(vR*.p0)', 'vL*'], ['(vL.p0)', 'vR'])
-            else:
+            else:  # left move or no move
                 theta = npc.tensordot(theta, self.RHeff, axes=['(p0.vR)', '(p0*.vL)'])
                 # 'vL', 'wL', '(p0.vL*)'
                 theta = npc.tensordot(self.LP, theta, axes=[['vR', 'wR'], ['vL', 'wL']])
@@ -933,7 +952,7 @@ class OneSiteH(EffectiveH):
             self.LHeff = env._contract_LHeff(self.i0, 'p0')
             self.pipeL = self.LHeff.get_leg('(vR*.p0)')
             self.acts_on = ['(vL.p0)', 'vR']
-        else:
+        else:  # left move or no move
             self.RHeff = env._contract_RHeff(self.i0, 'p0')
             self.pipeR = self.RHeff.get_leg('(p0.vL*)')
             self.acts_on = ['vL', '(p0.vR)']
@@ -954,7 +973,7 @@ class OneSiteH(EffectiveH):
         if self.combine:
             if self.move_right:
                 theta = theta.combine_legs(['vL', 'p0'], pipes=self.pipeL)
-            else:
+            else:  # left move or no move
                 theta = theta.combine_legs(['p0', 'vR'], pipes=self.pipeR)
         return theta.itranspose(self.acts_on)
 
@@ -965,7 +984,7 @@ class OneSiteH(EffectiveH):
                 contr = npc.tensordot(self.LHeff, self.RP, axes=['wR', 'wL'])
                 contr = contr.combine_legs([['(vR*.p0)', 'vL*'], ['(vR.p0*)', 'vL']],
                                            qconj=[+1, -1])
-            else:
+            else:  # left move or no move
                 contr = npc.tensordot(self.LP, self.RHeff, axes=['wR', 'wL'])
                 contr = contr.combine_legs([['vR*', '(p0.vL*)'], ['vR', '(p0*.vL)']],
                                            qconj=[+1, -1])
@@ -993,7 +1012,7 @@ class OneSiteH(EffectiveH):
 
     def update_LP(self, env, i, U=None):
         if self.combine and self.move_right:
-            assert i == self.i0 + 1  # TODO: hit this in single-site?!?
+            assert i == self.i0 + 1
             LP = npc.tensordot(self.LHeff, U, axes=['(vR.p0*)', '(vL.p)'])
             LP = npc.tensordot(U.conj(), LP, axes=['(vL*.p*)', '(vR*.p0)'])
             env.set_LP(i, LP, age=env.get_LP_age(i - 1) + 1)
@@ -1001,7 +1020,7 @@ class OneSiteH(EffectiveH):
             env.get_LP(i, store=True)
 
     def update_RP(self, env, i, VH=None):
-        if self.combine and (self.move_right is False):
+        if self.combine and not self.move_right:
             assert i == self.i0 - 1
             RP = npc.tensordot(VH, self.RHeff, axes=['(p.vR)', '(p0*.vL)'])
             RP = npc.tensordot(RP, VH.conj(), axes=['(p0.vL*)', '(p*.vR*)'])
@@ -1226,6 +1245,7 @@ class ZeroSiteH(EffectiveH):
         Number of (MPS) sites the effective hamiltonian covers.
     acts_on : list of str
         Labels of the state on which `self` acts. NB: class attribute.
+        Overwritten by normal attribute, if `combine`.wL', '(p0, vL*)'``
         Overwritten by normal attribute, if `combine`.
     LHeff, RHeff : :class:`~tenpy.linalg.np_conserved.Array`
         Only set if :attr:`combine`, and only one of them depending on :attr:`move_right`.
@@ -1476,7 +1496,7 @@ class Mixer:
 
         The LHS is equal to the RHS up to truncation and rescaling (we normalize to ``norm(S)==1``).
         The double lines (``===``) indicate the mixed/expanded bonds.
-        Only the tensor with a physical leg (e.g. `U` for a right mive) is an isometry and is
+        Only the tensor with a physical leg (e.g. `U` for a right move) is an isometry and is
         equivalent to the corresponding output of :meth:`mixed_svd_2site`.
         It carries the `qtotal` of `theta`.
         The other (e.g. `VH` for a right move) is in general not isometric.
@@ -1496,7 +1516,7 @@ class Mixer:
             The site that ``theta`` lives on. The bond to be expanded is ``i0, i0 + 1`` for a right
             move or ``i0 - 1, i0`` for a left move.
         move_right : bool | None
-            Whether we move to the right (``True``), left (``False``), or dont move (``None``).
+            Whether we move to the right (``True``), left (``False``), or don't move (``None``).
 
         Returns
         -------
@@ -2049,8 +2069,13 @@ class VariationalCompression(Sweep):
         This is done to ensure proper convergence after each sweep, even if that implies that
         the site 0 is then updated twice per sweep.
         """
-        extra = (0, True, [False, False])
-        return itertools.chain(super().get_sweep_schedule(), [extra])
+        extra = (0, None, [False, False])
+        schedule = list(super().get_sweep_schedule())
+        if schedule[-1] != extra:
+            schedule.append(extra)
+        return schedule
+        # TODO should we keep the above?
+        # return itertools.chain(super().get_sweep_schedule(), [extra])
 
     def update_local(self, _, optimize=True):
         """Perform local update.

tenpy/algorithms/tdvp.py
@@ -204,13 +204,14 @@ class TwoSiteTDVPEngine(TDVPEngine):
         # earlier update of environments, since they are needed for the one_site_update()
         super().update_env(**update_data)  # new environments, e.g. LP[i0+1] on right move.
 
-        if self.move_right:
-            # note that i0 == L-2 is left-moving
+        if self.move_right is True:  # and i0 != L-2, but that is left-moving
             self.one_site_update(i0 + 1, 0.5j * self.dt)
-        elif (self.move_right is False):
+        elif (self.move_right is False):  # and i0 != 0, but that has `move_right is None`
             self.one_site_update(i0, 0.5j * self.dt)
-        # for the last update of the sweep, where move_right is None, there is no one_site_update
-        
+        elif self.move_right is None:
+            pass
+        else:
+            assert False, "Invalid move_right={move_right!r}"
         return update_data
 
     def update_env(self, **update_data):
@@ -284,9 +285,9 @@ class SingleSiteTDVPEngine(TDVPEngine):
 
         # update one-site wavefunction
         theta, N = LanczosEvolution(self.eff_H, theta, self.lanczos_options).run(-0.5j * dt)
-        if self.move_right:
+        if self.move_right is True:
             self.right_moving_update(i0, theta)
-        else:
+        elif self.move_right is False or self.move_right is None:
             # note: left_moving_update() also covers the "non-moving" case move_right=None
             # of the last update in a sweep
             self.left_moving_update(i0, theta)
@@ -324,6 +325,7 @@ class SingleSiteTDVPEngine(TDVPEngine):
         self.psi.set_B(i0, B1, form='B')  # right-canonical
         self.psi.set_SL(i0, S)
 
+        assert (i0 == 0) == (self.move_right is None), "unexpected sweep schedule"
         if i0 != 0:  # left-moving, but not the last site of the update
             super().update_env(VH=VH)  # note: no update needed if i0=0!
             theta = U.iscale_axis(S, 'vR')
@@ -356,6 +358,7 @@ class TimeDependentSingleSiteTDVP(TimeDependentHAlgorithm,SingleSiteTDVPEngine):
     See details in :class:`~tenpy.algorithms.algorithm.TimeDependentHAlgorithm` as well.
     """
     def reinit_model(self):
+        # note: this fct is also used in TimeDependentTwoSiteTDVP
         # recreate model
         TimeDependentHAlgorithm.reinit_model(self)
         # and reinitializie environment accordingly
@@ -369,7 +372,7 @@ class TimeDependentTwoSiteTDVP(TimeDependentHAlgorithm,TwoSiteTDVPEngine):
     """
 
     def reinit_model(self):
-        TimeDependentSingleSiteTDVP.reinit_model(self)
+        TimeDependentSingleSiteTDVP.reinit_model(self)  # call the code a few lines above
 
 
 class OldTDVPEngine(TimeEvolutionAlgorithm):