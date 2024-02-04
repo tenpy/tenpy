b7f642b1f4f1b71f31b78449c25654f1e6663a33
jangeiger <62647585+jangeiger@users.noreply.github.com>
2024-02-04 16:52:53 +0100

    fixed docstring

--- tenpy/models/hubbard.py
+++ tenpy/models/hubbard.py
@@ -97,8 +97,6 @@ class BoseHubbardChain(BoseHubbardModel, NearestNeighborModel):
         -------
         factor : int
             saving factor, due to conservation
-
-
         """
         chinfo = self.lat.unit_cell[0].leg.chinfo
         savings = 1
