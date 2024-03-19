1aa390df2987acf4b1092ae28201b4619806f230
p-thoma <philipp.thoma@tum.de>
2024-03-19 09:42:33 +0100

    Refactor test_cs_projection
    Simplify initialization of sites and add comments to the test.
    Furthermore include a test for conservation of ``'N'`` for `FermionSite`

--- tests/test_cs_projection.py
+++ tests/test_cs_projection.py
@@ -1,66 +1,62 @@
-"""A test for tenpy.networks.mps._project_onto_sector_from_charge_tree"""
+"""A test for :meth:`~tenpy.networks.mps.MPS.project_onto_charge_sector`"""
 # Copyright (C) TeNPy Developers, GNU GPLv3
 
 import numpy as np
-import pytest
-#from tenpy.networks.site import SpinHalfSite
-from tenpy.networks.site import SpinSite
+from tenpy.networks.site import SpinSite, FermionSite
 from tenpy.networks.mps import MPS
 
 
 def test_cs_projection():
+    # define a product state (as used in MPS.from_product_state)
+    product_state_list = np.array([[1, 1],
+                                   [-1, 1],
+                                   [1, 1]]) / np.sqrt(2)
 
+    # define a list of sites (with conservation of the desired charge)
+    sites = [SpinSite(S=0.5, conserve='Sz')]*len(product_state_list) 
+    
+    # :meth:`MPS.project_onto_charge_sector` calls
+    # :meth:`MPS.get_charge_tree_for_given_charge_sector` and 
+    # :meth:`MPS._project_onto_sector_from_charge_tree`
+    mps_projected = MPS.project_onto_charge_sector(sites, product_state_list, charge_sector=(1,))
+    
+    expval = mps_projected.expectation_value('Sz').sum()
+    # check that we are in the given charge-sector (note that ints are used for half-integer spins)
+    assert np.isclose(expval, 0.5)
 
-    spin_site = SpinSite(S=0.5, conserve=None)
-    product_state_list = [[1 / np.sqrt(2), 1 / np.sqrt(2)], [-1 / np.sqrt(2), 1 / np.sqrt(2)], [1 / np.sqrt(2), 1 / np.sqrt(2)]]
-    MPS_from_prodstate = MPS.from_product_state([spin_site] * 3, product_state_list)
+    # test for different charge sector
+    mps_projected = MPS.project_onto_charge_sector(sites, product_state_list, charge_sector=(-1,))
+    expval = mps_projected.expectation_value('Sz').sum()
 
-    sites = MPS_from_prodstate.sites
+    assert np.isclose(expval, -0.5)
 
-    new_sites = []
-    for site in sites:
-        site_new = site.__class__(S=site.S, conserve='Sz')
-        new_sites.append(site_new)
+    # test for spin 1
+    product_state_list = np.array([[1, 1, 1],
+                                   [1, 1, 1],
+                                   [1, 1, 1]])/np.sqrt(3)
 
-    tree = MPS.get_charge_tree_for_given_charge_sector(new_sites, charge_sector=(1,))
-    res = MPS._project_onto_sector_from_charge_tree(new_sites,product_state_list, tree)
-    expval=res.expectation_value('Sz').sum()
+    sites = [SpinSite(S=1, conserve='Sz')]*len(product_state_list)
+ 
+    mps_projected = MPS.project_onto_charge_sector(sites, product_state_list, charge_sector=(2,))
+    expval = mps_projected.expectation_value('Sz').sum()
 
-    assert np.abs(expval -1/2)< 10**14
+    assert np.isclose(expval, 1)
 
-    tree = MPS.get_charge_tree_for_given_charge_sector(new_sites, charge_sector=(-1,))
-    res = MPS._project_onto_sector_from_charge_tree(new_sites,product_state_list, tree)
-    expval=res.expectation_value('Sz').sum()
+    mps_projected = MPS.project_onto_charge_sector(sites, product_state_list, charge_sector=(-2,))
+    expval = mps_projected.expectation_value('Sz').sum()
 
-    assert np.abs(expval +1/2)< 10**14
+    assert np.isclose(expval, -1)
 
+    # test conservation of ``'N'`` for fermions
+    product_state_list = np.array([[1, 1, 1, 1], 
+                                   [1, 1, 1, 1],
+                                   [1, 1, 1, 1]]) / 2
 
+    sites = [FermionSite()]*len(product_state_list)
+    mps_projected = MPS.project_onto_charge_sector(sites, product_state_list, charge_sector=(2,))
 
-    spin_site = SpinSite(S=1, conserve=None)
-    product_state_list = [[1/np.sqrt(3),1/np.sqrt(3), 1/np.sqrt(3)], [1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)], [1/np.sqrt(3),1/np.sqrt(3), 1/np.sqrt(3)]]
-    MPS_from_prodstate = MPS.from_product_state([spin_site] * 3, product_state_list)
+    assert np.isclose(mps_projected.expectation_value('N').sum(), 2)
 
-    sites = MPS_from_prodstate.sites
+    mps_projected = MPS.project_onto_charge_sector(sites, product_state_list, charge_sector=(0,))
 
-    new_sites = []
-    for site in sites:
-        site_new = site.__class__(S=site.S, conserve='Sz')
-        new_sites.append(site_new)
-
-    tree = MPS.get_charge_tree_for_given_charge_sector(new_sites, charge_sector=(2,))
-    res = MPS._project_onto_sector_from_charge_tree(new_sites, product_state_list, tree)
-    expval = res.expectation_value('Sz').sum()
-
-    assert np.abs(expval -1) < 10 ** 14
-
-
-    tree = MPS.get_charge_tree_for_given_charge_sector(new_sites, charge_sector=(-2,))
-    res = MPS._project_onto_sector_from_charge_tree(new_sites, product_state_list, tree)
-    expval = res.expectation_value('Sz').sum()
-
-    assert np.abs(expval +1) < 10 ** 14
-
-
-
-
-
+    assert np.isclose(mps_projected.expectation_value('N').sum(), 0)
