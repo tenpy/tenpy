7bd9209bb89b2958574432fb146f143fcad225fc
Jakob Unfried <jakob.unfried@tum.de>
2024-04-02 18:28:03 +0200

    Add stubs to nonabelian backend

--- tenpy/linalg/backends/nonabelian.py
+++ tenpy/linalg/backends/nonabelian.py
@@ -1,7 +1,7 @@
 # Copyright 2023-2023 TeNPy Developers, GNU GPLv3
 from __future__ import annotations
 from abc import ABC
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 from math import prod
 import numpy as np
 
@@ -141,9 +141,9 @@ class NonAbelianData:
         return block[idx1, idx2]
 
 
-# TODO do we need to inherit from ABC again??
+# TODO do we need to inherit from ABC again?? (same in abelian and no_symmetry)
 # TODO eventually remove BlockBackend inheritance, it is not needed,
-#  jakob only keeps it around to make his IDE happy
+#      jakob only keeps it around to make his IDE happy  (same in abelian and no_symmetry)
 class NonabelianBackend(Backend, BlockBackend, ABC):
     
     DataCls = NonAbelianData
@@ -172,6 +172,15 @@ class NonabelianBackend(Backend, BlockBackend, ABC):
             # check matching dtype
             assert self.block_dtype(block) == a.data.dtype
 
+    def test_mask_sanity(self, a: Mask):
+        raise NotImplementedError  # TODO
+
+    # TODO do we need leg metadata?
+    #  related methods:
+    #   - test_leg_sanity
+    #   - _fuse_spaces
+    #   - add_leg_metadata
+
     def get_dtype_from_data(self, a: NonAbelianData) -> Dtype:
         return a.dtype
 
@@ -183,15 +192,11 @@ class NonabelianBackend(Backend, BlockBackend, ABC):
         # supports all symmetries
         return isinstance(symmetry, Symmetry)
 
-    def is_real(self, a: BlockDiagonalTensor) -> bool:
-        # TODO (JU) this may not even be well-defined if a.symmetry is not a group ...
-        #  I think we should take the POV that we are always working over the complex numbers,
-        #  even if the dtype is real. in that case, imaginary parts just happen to be 0.
-        raise NotImplementedError
-
     def data_item(self, a: NonAbelianData) -> float | complex:
         if len(a.blocks) > 1:
-            raise ValueError('Not a scalar.')
+            raise ValueError("More than 1 block!")
+        if len(a.blocks) == 0:
+            return a.dtype.zero_scalar
         return self.block_item(a.blocks[0])
 
     def to_dense_block(self, a: BlockDiagonalTensor) -> Block:
@@ -219,6 +224,9 @@ class NonabelianBackend(Backend, BlockBackend, ABC):
                     res[idcs] += contribution
         return res
 
+    def diagonal_to_block(self, a: DiagonalTensor) -> Block:
+        raise NotImplementedError  # TODO
+
     def fusion_tree_to_block(self, tree: FusionTree) -> Block:
         """convert a FusionTree to a block, containing its "matrix" representation."""
         raise NotImplementedError  # TODO
@@ -227,6 +235,12 @@ class NonabelianBackend(Backend, BlockBackend, ABC):
                          rtol: float = 0.00001) -> NonAbelianData:
         # TODO add arg to specify (co-)domain?
         raise NotImplementedError
+
+    def diagonal_from_block(self, a: Block, leg: VectorSpace) -> DiagonalData:
+        raise NotImplementedError  # TODO
+
+    def mask_from_block(self, a: Block, large_leg: VectorSpace, small_leg: VectorSpace) -> DiagonalData:
+        raise NotImplementedError  # TODO
 
     def from_block_func(self, func, legs: list[VectorSpace], func_kwargs={}) -> NonAbelianData:
         # TODO add arg to specify (co-)domain?
@@ -242,12 +256,18 @@ class NonabelianBackend(Backend, BlockBackend, ABC):
         dtype = self.block_dtype(sample_block)
         return NonAbelianData(coupled, blocks, domain, codomain, dtype)
 
+    def diagonal_from_block_func(self, func, leg: VectorSpace, func_kwargs={}) -> DiagonalData:
+        raise NotImplementedError  # TODO
+
     def zero_data(self, legs: list[VectorSpace], dtype: Dtype) -> NonAbelianData:
         # TODO add arg to specify (co-)domain?
         codomain = ProductSpace(legs, backend=self).as_flat_product()
         domain = ProductSpace([], backend=self)
         return NonAbelianData(coupled_sectors=codomain.symmetry.empty_sector_array, blocks=[],
                               domain=domain, codomain=codomain, dtype=dtype)
+
+    def zero_diagonal_data(self, leg: VectorSpace, dtype: Dtype) -> DiagonalData:
+        raise NotImplementedError  # TODO
 
     def eye_data(self, legs: list[VectorSpace], dtype: Dtype) -> NonAbelianData:
         # TODO add arg to specify (co-)domain?
@@ -270,4 +290,153 @@ class NonabelianBackend(Backend, BlockBackend, ABC):
                          max_lines: int) -> list[str]:
         raise NotImplementedError  # TODO
 
-    # TODO implement all backend methods
+    def tdot(self, a: BlockDiagonalTensor, b: BlockDiagonalTensor, axs_a: list[int],
+             axs_b: list[int]) -> Data:
+        # TODO need to be able to specify levels of braiding in general case!
+        # TODO offer separate planar version? or just high
+        raise NotImplementedError  # TODO
+
+    def svd(self, a: BlockDiagonalTensor, new_vh_leg_dual: bool, algorithm: str | None,
+            compute_u: bool, compute_vh: bool) -> tuple[Data, DiagonalData, Data, VectorSpace]:
+        # TODO need to redesign Backend.svd specification! need to allow more than two legs!
+        # TODO need to be able to specify levels of braiding in general case!
+        raise NotImplementedError  # TODO
+
+    def qr(self, a: BlockDiagonalTensor, new_r_leg_dual: bool, full: bool
+           ) -> tuple[Data, Data, VectorSpace]:
+        # TODO do SVD first, comments there apply.
+        raise NotImplementedError  # TODO
+
+    def outer(self, a: BlockDiagonalTensor, b: BlockDiagonalTensor) -> Data:
+        # TODO what target leg order is easiest? does it match the one specified in Backend.outer?
+        raise NotImplementedError  # TODO
+
+    def inner(self, a: BlockDiagonalTensor, b: BlockDiagonalTensor, do_conj: bool,
+              axs2: list[int] | None) -> complex:
+        raise NotImplementedError  # TODO
+    
+    def permute_legs(self, a: BlockDiagonalTensor, permutation: list[int]) -> Data:
+        # TODO need to specify levels for braiding and partitioning into domain / codomain
+        raise NotImplementedError  # TODO
+
+    def trace_full(self, a: BlockDiagonalTensor, idcs1: list[int], idcs2: list[int]
+                   ) -> float | complex:
+        raise NotImplementedError  # TODO
+
+    def trace_partial(self, a: BlockDiagonalTensor, idcs1: list[int], idcs2: list[int],
+                      remaining_idcs: list[int]) -> Data:
+        raise NotImplementedError  # TODO
+
+    def diagonal_tensor_trace_full(self, a: DiagonalTensor) -> float | complex:
+        raise NotImplementedError  # TODO
+
+    def conj(self, a: BlockDiagonalTensor | DiagonalTensor) -> Data | DiagonalData:
+        # TODO what does this even mean? transpose of dagger?
+        # TODO should we offer transpose and dagger too?
+        raise NotImplementedError  # TODO
+
+    def combine_legs(self, a: BlockDiagonalTensor, combine_slices: list[int, int],
+                     product_spaces: list[ProductSpace], new_axes: list[int],
+                     final_legs: list[VectorSpace]) -> Data:
+        raise NotImplementedError  # TODO
+        
+    def split_legs(self, a: BlockDiagonalTensor, leg_idcs: list[int],
+                   final_legs: list[VectorSpace]) -> Data:
+        # TODO do we need metadata to split, like in abelian?
+        raise NotImplementedError  # TODO
+
+    def add_trivial_leg(self, a: BlockDiagonalTensor, pos: int) -> Data:
+        raise NotImplementedError  # TODO
+
+    def almost_equal(self, a: BlockDiagonalTensor, b: BlockDiagonalTensor, rtol: float, atol: float
+                     ) -> bool:
+        raise NotImplementedError  # TODO
+
+    def squeeze_legs(self, a: BlockDiagonalTensor, idcs: list[int]) -> Data:
+        raise NotImplementedError  # TODO
+
+    def norm(self, a: BlockDiagonalTensor | DiagonalTensor, order: int | float = None) -> float:
+        # TODO be careful about weight with quantum dimensions! probably only support 2 norm.
+        raise NotImplementedError  # TODO
+
+    def act_block_diagonal_square_matrix(self, a: BlockDiagonalTensor,
+                                         block_method: Callable[[Block], Block]) -> Data:
+        raise NotImplementedError  # TODO
+
+    def add(self, a: BlockDiagonalTensor, b: BlockDiagonalTensor) -> Data:
+        raise NotImplementedError  # TODO
+
+    def mul(self, a: float | complex, b: BlockDiagonalTensor) -> Data:
+        raise NotImplementedError  # TODO
+
+    def infer_leg(self, block: Block, legs: list[VectorSpace | None], is_dual: bool = False,
+                  is_real: bool = False) -> VectorSpace:
+        raise NotImplementedError  # TODO
+
+    def get_element(self, a: BlockDiagonalTensor, idcs: list[int]) -> complex | float | bool:
+        raise NotImplementedError  # TODO
+
+    def get_element_diagonal(self, a: DiagonalTensor, idx: int) -> complex | float | bool:
+        raise NotImplementedError  # TODO
+
+    def set_element(self, a: BlockDiagonalTensor, idcs: list[int], value: complex | float) -> Data:
+        # TODO not sure this can even be done sensibly, one entry of the dense block
+        #      affects in general many entries of the blocks.
+        raise NotImplementedError  # TODO
+
+    def set_element_diagonal(self, a: DiagonalTensor, idx: int, value: complex | float | bool
+                             ) -> DiagonalData:
+        # TODO not sure this can even be done sensibly, one entry of the dense block
+        #      affects in general many entries of the blocks.
+        raise NotImplementedError  # TODO
+
+    def diagonal_data_from_full_tensor(self, a: BlockDiagonalTensor, check_offdiagonal: bool
+                                       ) -> DiagonalData:
+        raise NotImplementedError  # TODO
+
+    def full_data_from_diagonal_tensor(self, a: DiagonalTensor) -> Data:
+        raise NotImplementedError  # TODO
+
+    def full_data_from_mask(self, a: Mask, dtype: Dtype) -> Data:
+        raise NotImplementedError  # TODO
+
+    def scale_axis(self, a: BlockDiagonalTensor, b: DiagonalTensor, leg: int) -> Data:
+        raise NotImplementedError  # TODO
+
+    # TODO how does entropy work here, should it consider quantum dimensions?
+
+    def diagonal_elementwise_unary(self, a: DiagonalTensor, func, func_kwargs,
+                                   maps_zero_to_zero: bool) -> DiagonalData:
+        raise NotImplementedError  # TODO
+
+    def diagonal_elementwise_binary(self, a: DiagonalTensor, b: DiagonalTensor, func,
+                                    func_kwargs, partial_zero_is_zero: bool) -> DiagonalData:
+        raise NotImplementedError  # TODO
+
+    def apply_mask_to_Tensor(self, tensor: BlockDiagonalTensor, mask: Mask, leg_idx: int) -> Data:
+        raise NotImplementedError  # TODO
+
+    def apply_mask_to_DiagonalTensor(self, tensor: DiagonalTensor, mask: Mask) -> DiagonalData:
+        raise NotImplementedError  # TODO
+
+    def eigh(self, a: BlockDiagonalTensor, sort: str = None) -> tuple[DiagonalData, Data]:
+        # TODO do SVD first, comments there apply.
+        raise NotImplementedError  # TODO
+
+    def from_flat_block_trivial_sector(self, block: Block, leg: VectorSpace) -> Data:
+        raise NotImplementedError  # TODO
+
+    def to_flat_block_trivial_sector(self, tensor: BlockDiagonalTensor) -> Block:
+        raise NotImplementedError  # TODO
+
+    def inv_part_from_flat_block_single_sector(self, block: Block, leg: VectorSpace,
+                                               dummy_leg: VectorSpace) -> Data:
+        raise NotImplementedError  # TODO
+
+    def inv_part_to_flat_block_single_sector(self, tensor: BlockDiagonalTensor) -> Block:
+        raise NotImplementedError  # TODO
+
+    def flip_leg_duality(self, tensor: BlockDiagonalTensor, which_legs: list[int],
+                         flipped_legs: list[VectorSpace], perms: list[np.ndarray]) -> Data:
+        # TODO think carefully about what this means.
+        raise NotImplementedError  # TODO
