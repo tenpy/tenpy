f3c86f142ece2d4fa6a42a5fc81a704482a6fa66
MartinHefel <ge49suc@mytum.de>
2024-05-20 18:36:55 +0200

    implement side specific qr decomposition
    allowing to decompose ``theta=A@Th`` or ``theta=Th@B`` without necessarily computing ``Th``.

--- tenpy/algorithms/truncation.py
+++ tenpy/algorithms/truncation.py
@@ -307,74 +307,133 @@ def svd_theta(theta, trunc_par, qtotal_LR=[None, None], inner_labels=['vR', 'vL'
     return U, S, VH, err, renormalization
 
 
-def _qr_theta_Y0(B_L: npc.Array, B_R: npc.Array, theta: npc.Array, expand: float, min_block_increase: int):
-    """Generate the initial guess Y0 for the right isometry for the QR based theta decomposition decompose_theta_qr_based().
+def _qr_theta_Y0(B_L: npc.Array, B_R: npc.Array, theta: npc.Array, get_left_side: bool, expand: float, min_block_increase: int):
+    """Generate the initial guess `Y0` for the (left) right isometry for the QR based theta decomposition decompose_theta_qr_based().
 
     Parameters
     ----------
     B_L : Array with legs [vL, p, vR]
     B_R : Array with legs [vL, p, vR]
     theta : Array with legs [(vL.p0), (p1.vR)]
+    get_left_side : bool 
     expand : float or None
+    min_block_increase : int
 
     Returns
     -------
-    Y0 : Array with legs [vL, (p1.vR)]
+    Y0 : Array with legs [vL, (p1.vR)] or [(vL.p0), vR]
     """
+    # TODO: Write code more compact, after method is double checked
+
     if expand is None or expand == 0:
-        return B_R.combine_legs(['p', 'vR']).ireplace_labels('(p.vR)', '(p1.vR)')
+        if get_left_side:
+            return B_L.combine_legs(['vL', 'p']).ireplace_labels('(vL.p)', '(vL.p0)')
+        else:
+            return B_R.combine_legs(['p', 'vR']).ireplace_labels('(p.vR)', '(p1.vR)')
 
     assert min_block_increase >= 0
 
-    Y0 = theta.copy(deep=False)
-    Y0.legs[0] = Y0.legs[0].to_LegCharge()
-    Y0.ireplace_label('(vL.p0)', 'vL')
-    if any(B_L.qtotal != 0):
-        Y0.gauge_total_charge('vL', new_qtotal=B_R.qtotal)
-    vL_old = B_R.get_leg('vL')
-    if not vL_old.is_blocked():
-        vL_old = vL_old.sort()[1]
-    vL_new = Y0.get_leg('vL')  # is blocked, since created from pipe
+    
+    if get_left_side:
+        Y0 = theta.copy(deep=False)
+        Y0.legs[1] = Y0.legs[1].to_LegCharge()
+        Y0.ireplace_label('(p1.vR)', 'vR')
+        if any(B_R.qtotal != 0): # <- TODO: Acessing the B_R(i+1) tensor could be a problem, if i+1 is saved as C tensor (if EIG based SVD)?
+            Y0.gauge_total_charge('vR', new_qtotal=B_L.qtotal)
+        vR_old = B_L.get_leg('vR')
+        if not vR_old.is_blocked():
+            vR_old = vR_old.sort()[1]
+        vR_new = Y0.get_leg('vR')  # is blocked, since created from pipe
+    else:
+        Y0 = theta.copy(deep=False)
+        Y0.legs[0] = Y0.legs[0].to_LegCharge()
+        Y0.ireplace_label('(vL.p0)', 'vL')
+        if any(B_L.qtotal != 0):
+            Y0.gauge_total_charge('vL', new_qtotal=B_R.qtotal)
+        vL_old = B_R.get_leg('vL')
+        if not vL_old.is_blocked():
+            vL_old = vL_old.sort()[1]
+        vL_new = Y0.get_leg('vL')  # is blocked, since created from pipe
 
-    # vL_old is guaranteed to be a slice of vL_new by charge rule in B_L
-    piv = np.zeros(vL_new.ind_len, dtype=bool)  # indices to keep in vL_new
-    increase_per_block = max(min_block_increase, int(vL_old.ind_len * expand // vL_new.block_number))
-    sizes_old = vL_old.get_block_sizes()
-    sizes_new = vL_new.get_block_sizes()
-    # iterate over charge blocks in vL_new and vL_old at the same time
-    j_old = 0
-    q_old = vL_old.charges[j_old, :]
-    qdata_order = np.argsort(Y0._qdata[:, 0])
-    qdata_idx = 0
-    for j_new, q_new in enumerate(vL_new.charges):
-        if all(q_new == q_old):  # have charge block in both vL_new and vL_old
-            s_new = sizes_old[j_old] + increase_per_block
-            # move to next charge block in next loop iteration
-            j_old += 1
-            if j_old < len(vL_old.charges):
-                q_old = vL_old.charges[j_old, :]
-        else:  # charge block only in vL_new
-            s_new = increase_per_block
-        s_new = min(s_new, sizes_new[j_new])  # don't go beyond block
+    # vL(R)_old is guaranteed to be a slice of vL(R)_new by charge rule in B_L(R)
+    if get_left_side:
+        piv = np.zeros(vR_new.ind_len, dtype=bool)  # indices to keep in vR_new
+        increase_per_block = max(min_block_increase, int(vR_old.ind_len * expand // vR_new.block_number))
+        sizes_old = vR_old.get_block_sizes()
+        sizes_new = vR_new.get_block_sizes()
+    else:
+        piv = np.zeros(vL_new.ind_len, dtype=bool)  # indices to keep in vL_new
+        increase_per_block = max(min_block_increase, int(vL_old.ind_len * expand // vL_new.block_number))
+        sizes_old = vL_old.get_block_sizes()
+        sizes_new = vL_new.get_block_sizes()
 
-        if Y0._qdata[qdata_order[qdata_idx], 0] != j_new:
-            # block does not exist
-            # while we could set corresponding piv entries to True, it would not help, since
-            # the corresponding "entries" of Y0 are zero anyway
-            continue
+    # iterate over charge blocks in vL(R)_new and vL(R)_old at the same time
+    if get_left_side:
+        j_old = 0
+        q_old = vR_old.charges[j_old, :]
+        qdata_order = np.argsort(Y0._qdata[:, 1]) # <- CHANGED 0 to 1 compared to get_left_side=False
+        qdata_idx = 0
+        for j_new, q_new in enumerate(vR_new.charges):
+            if all(q_new == q_old):  # have charge block in both vR_new and vR_old
+                s_new = sizes_old[j_old] + increase_per_block
+                # move to next charge block in next loop iteration
+                j_old += 1
+                if j_old < len(vR_old.charges):
+                    q_old = vR_old.charges[j_old, :]
+            else:  # charge block only in vR_new
+                s_new = increase_per_block
+            s_new = min(s_new, sizes_new[j_new])  # don't go beyond block
 
-        # block has axis [vL, (p1.vR)]. want to keep the s_new slices of the vL axis
-        #  that have the largest norm
-        norms = np.linalg.norm(Y0._data[qdata_order[qdata_idx]], axis=1)
-        kept_slices = np.argsort(-norms)[:s_new]  # negative sign so we sort large to small
-        start = vL_new.slices[j_new]
-        piv[start + kept_slices] = True
+            if Y0._qdata[qdata_order[qdata_idx], 1] != j_new: # <- CHANGED 0 to 1 compared to get_left_side=False
+                # block does not exist
+                # while we could set corresponding piv entries to True, it would not help, since
+                # the corresponding "entries" of Y0 are zero anyway
+                continue
 
-        qdata_idx += 1
-        if qdata_idx >= Y0._qdata.shape[0]:
-            break
+            # block has axis [(vL.p0),vR]. want to keep the s_new slices of the vR axis
+            #  that have the largest norm
+            norms = np.linalg.norm(Y0._data[qdata_order[qdata_idx]], axis=0) # <- CHANGED axis compared to get_left_side=False
+            kept_slices = np.argsort(-norms)[:s_new]  # negative sign so we sort large to small
+            start = vR_new.slices[j_new]
+            piv[start + kept_slices] = True
 
-    Y0.iproject(piv, 'vL')
+            qdata_idx += 1
+            if qdata_idx >= Y0._qdata.shape[0]:
+                break
+        Y0.iproject(piv, 'vR')
+    else: 
+        j_old = 0
+        q_old = vL_old.charges[j_old, :]
+        qdata_order = np.argsort(Y0._qdata[:, 0])
+        qdata_idx = 0
+        for j_new, q_new in enumerate(vL_new.charges):
+            if all(q_new == q_old):  # have charge block in both vL_new and vL_old
+                s_new = sizes_old[j_old] + increase_per_block
+                # move to next charge block in next loop iteration
+                j_old += 1
+                if j_old < len(vL_old.charges):
+                    q_old = vL_old.charges[j_old, :]
+            else:  # charge block only in vL_new
+                s_new = increase_per_block
+            s_new = min(s_new, sizes_new[j_new])  # don't go beyond block
+
+            if Y0._qdata[qdata_order[qdata_idx], 0] != j_new:
+                # block does not exist
+                # while we could set corresponding piv entries to True, it would not help, since
+                # the corresponding "entries" of Y0 are zero anyway
+                continue
+
+            # block has axis [vL, (p1.vR)]. want to keep the s_new slices of the vL axis
+            #  that have the largest norm
+            norms = np.linalg.norm(Y0._data[qdata_order[qdata_idx]], axis=1)
+            kept_slices = np.argsort(-norms)[:s_new]  # negative sign so we sort large to small
+            start = vL_new.slices[j_new]
+            piv[start + kept_slices] = True
+
+            qdata_idx += 1
+            if qdata_idx >= Y0._qdata.shape[0]:
+                break
+        Y0.iproject(piv, 'vL')
     return Y0
 
 
@@ -439,71 +498,148 @@ def _eig_based_svd(A, need_U: bool = True, need_Vd: bool = True, inner_labels=[N
     return U, S, Vd, trunc_err, renormalize
 
 
-def decompose_theta_qr_based(B_L: npc.Array, B_R: npc.Array, theta: npc.Array, trunc_params: dict, 
-             expand: float, use_eig_based_svd: bool, need_A_L: bool, 
-             compute_err: bool, min_block_increase: int):
-    """Performs a QR based decomposition of a matrix `theta` (= the wavefunction) and truncates it
+def decompose_theta_qr_based(old_B_L: npc.Array, old_B_R: npc.Array, theta: npc.Array, get_left_side: bool,
+                             expand: float, min_block_increase: int, use_eig_based_svd: bool, trunc_params: dict, compute_err: bool, 
+                             need_other_side: bool):
+    """Performs a QR based decomposition of a matrix `theta` (= the wavefunction) and truncates it. 
+    The result is an approximation.
 
+    The decomposition for ``use_eig_based_svd=False`` is::
+
+        |   -- theta --   ==>   -- t_L --- S --- t_R --
+        |      |   |                |             |
+
+    Where `t_L` is in `'A'` form and `t_R` in `'B'` form.
+
+    The decomposition for ``use_eig_based_svd=True`` is::
+
+        |   -- theta --   ==>   -- t_L --- t_R --
+        |      |   |                |       |
+
+    Where `t_L` is in `'A'` (`'Th'`) form and `t_R` in `'Th'` (`'B'`) form, if ``get_left_side=True`` 
+    (``get_left_side=False``).
+    
     Parameters
     ----------
-    B_L : Array with legs [vL, p, vR]
-    B_R : Array with legs [vL, p, vR]
-    theta : Array with legs [(vL.p0), (p1.vR)]
+    old_B_L : npc.Array
+        Array with legs [vL, p, vR]
+    old_B_R : npc.Array
+        Array with legs [vL, p, vR]
+    theta : npc.Array
+        Array with legs [(vL.p0), (p1.vR)]
+    expand : float | None
+        Expansion rate. The QR-based decomposition is carried out at an expanded bond dimension.
+    min_block_increase : int
+        Minimum bond dimension increase for each block.
+    get_left_side : bool 
+        Whether the left (`t_L`) or the right (`t_R`) matrix should be calculated.
+    use_eig_based_svd : bool
+        Whether the SVD of the bond matrix :math:`\Xi` should be carried out numerically via
+        the eigensystem. This is faster on GPUs, but less accurate.
+        It makes no sense to do this on CPU.
     trunc_par : dict
         truncation parameters as described in :func:`truncate`.
-    ...
+    compute_err : bool
+        Whether the truncation error should be computed exactly.
+        Computing the truncation error is significantly more expensive.
+        If `True`, the full error is computed.
+        Otherwise, the truncation error is set to NaN.
+    need_other_side : bool
+        Whether the other matrix (``not get_left_side``) should be calculated.
+        If ``compute_err=True`` => ``need_other_side=True``.
 
     Returns
     -------
-    A_L : array with legs [(vL.p), vR] or None
+    t_L : array with legs [(vL.p), vR] or None
     S : 1D numpy array
-    B_R : array with legs [vL, (p.vR)]
+    t_R : array with legs [vL, (p.vR)] or None
+    form : list containing two entries providing the form of the two arrays `t_L` and `t_R`
     trunc_err : TruncationError
     renormalize : float
     """
-    # TODO: Do we need to implement a parameter inner_labels as for the svd method?
 
     if compute_err:
-        need_A_L = True
+        need_other_side = True
 
-    # Get inital guess for the right isometry
-    Y0 = _qr_theta_Y0(B_L, B_R, theta, expand, min_block_increase) # Y0: [vL, (p1.vR)]
+    if get_left_side:
+        # Get inital guess for the left isometry
+        Y0 = _qr_theta_Y0(old_B_L, old_B_R, theta, get_left_side, expand, min_block_increase) # Y0: [(vL.p0), vR]
 
-    # QR based updates
-    theta_i0 = npc.tensordot(theta, Y0.conj(), ['(p1.vR)', '(p1*.vR*)']).ireplace_label('vL*', 'vR')
-    A_L, _ = npc.qr(theta_i0, inner_labels=['vR', 'vL']) # A_L: [(vL.p0), vR]
+        # QR based updates
+        theta_i1 = npc.tensordot(Y0.conj(), theta, ['(vL*.p0*)', '(vL.p0)']).ireplace_label('vR*', 'vL') # theta_i1: [vL,(p1.vR)]
+        theta_i1.itranspose(['(p1.vR)', 'vL']) # theta_i1: [(p1.vR),vL]
+        B_R, _ = npc.qr(theta_i1, inner_labels=['vL', 'vR'], inner_qconj=-1) # B_R: [(p1.vR),vL] 
+        B_R.itranspose(['vL', '(p1.vR)']) # B_R: [vL,(p1.vR)] 
 
-    theta_i1 = npc.tensordot(A_L.conj(), theta, ['(vL*.p0*)', '(vL.p0)']).ireplace_label('vR*', 'vL')
-    theta_i1.itranspose(['(p1.vR)', 'vL'])
-    B_R, Xi = npc.qr(theta_i1, inner_labels=['vL', 'vR'], inner_qconj=-1)
-    B_R.itranspose(['vL', '(p1.vR)'])
-    Xi.itranspose(['vL', 'vR'])
+        theta_i0 = npc.tensordot(theta, B_R.conj(), ['(p1.vR)', '(p1*.vR*)']).ireplace_label('vL*', 'vR') # theta_i0: [(vL.p0),vR]
+        A_L, Xi = npc.qr(theta_i0, inner_labels=['vR', 'vL']) # A_L: [(vL.p0), vR]
+        
+    else:
+        # Get inital guess for the right isometry
+        Y0 = _qr_theta_Y0(old_B_L, old_B_R, theta, get_left_side, expand, min_block_increase) # Y0: [vL, (p1.vR)]
+
+        # QR based updates
+        theta_i0 = npc.tensordot(theta, Y0.conj(), ['(p1.vR)', '(p1*.vR*)']).ireplace_label('vL*', 'vR') # theta_i0: [(vL.p0),vR]
+        A_L, _ = npc.qr(theta_i0, inner_labels=['vR', 'vL']) # A_L: [(vL.p0), vR]
+
+        theta_i1 = npc.tensordot(A_L.conj(), theta, ['(vL*.p0*)', '(vL.p0)']).ireplace_label('vR*', 'vL') # theta_i1: [vL,(p1.vR)]
+        theta_i1.itranspose(['(p1.vR)', 'vL']) # theta_i1: [(p1.vR),vL]
+        B_R, Xi = npc.qr(theta_i1, inner_labels=['vL', 'vR'], inner_qconj=-1)
+        B_R.itranspose(['vL', '(p1.vR)'])
+        Xi.itranspose(['vL', 'vR'])
 
     # SVD of bond matrix Xi
     if use_eig_based_svd:
         U, S, Vd, trunc_err, renormalize = _eig_based_svd(
-            Xi, inner_labels=['vR', 'vL'], need_U=need_A_L, trunc_params=trunc_params
+            Xi, need_U=get_left_side, need_Vd=(not get_left_side), inner_labels=['vR', 'vL'], trunc_params=trunc_params
         )
     else:
-        U, S, Vd, _, renormalize = svd_theta(Xi, trunc_params)
-    B_R = npc.tensordot(Vd, B_R, ['vR', 'vL'])
-    if need_A_L:
-        A_L = npc.tensordot(A_L, U, ['vR', 'vL'])
-    else:
-        A_L = None
+        U, S, Vd, _, renormalize = svd_theta(Xi, trunc_params) # <- TODO: Is it fine to not specify the charges here?
 
+    # Asign return matrices
+    t_L, t_R = None, None
+    form = ['A','B']
+    if get_left_side:
+        t_L = npc.tensordot(A_L, U, ['vR', 'vL'])
+        if need_other_side:
+            if use_eig_based_svd:
+                t_R = npc.tensordot(Xi, B_R, ['vR', 'vL'])
+                t_R = npc.tensordot(U.iconj(), t_R, ['vL*', 'vL']).ireplace_label('vR*', 'vL')
+                form[1] = 'Th'
+            else:
+                t_R = npc.tensordot(Vd, B_R, ['vR', 'vL'])
+    else:
+        t_R = npc.tensordot(Vd, B_R, ['vR', 'vL'])
+        if need_other_side:
+            if use_eig_based_svd:
+                t_L = npc.tensordot(A_L, Xi, ['vR', 'vL'])
+                t_L = npc.tensordot(t_L, Vd.iconj(), ['vR', 'vR*']).ireplace_label('vL*', 'vR')
+                form[0] = 'Th'
+            else:
+                t_L = npc.tensordot(A_L, U, ['vR', 'vL'])
+
+    # Compute error
     if compute_err:
-        theta_approx = npc.tensordot(A_L.scale_axis(S, axis='vR'), B_R, ['vR', 'vL'])
+        if use_eig_based_svd:
+            theta_approx = npc.tensordot(t_L, t_R, ['vR', 'vL'])
+        else:
+            theta_approx = npc.tensordot(t_L.scale_axis(S, axis='vR'), t_R, ['vR', 'vL'])
         eps = npc.norm(theta - theta_approx) ** 2
         trunc_err = TruncationError(eps, 1. - 2. * eps)
     else:
         trunc_err = TruncationError(np.nan, np.nan)
 
-    B_R = B_R.ireplace_label('(p1.vR)', '(p.vR)')
-    if need_A_L:
-        A_L = A_L.ireplace_label('(vL.p0)', '(vL.p)')
-
-    return A_L, S, B_R, trunc_err, renormalize
+    # Replace labels
+    if get_left_side:
+        t_L = t_L.ireplace_label('(vL.p0)', '(vL.p)')
+        if need_other_side:
+            t_R = t_R.ireplace_label('(p1.vR)', '(p.vR)')
+    else:
+        t_R = t_R.ireplace_label('(p1.vR)', '(p.vR)')
+        if need_other_side:
+            t_L = t_L.ireplace_label('(vL.p0)', '(vL.p)')
+        
+    return t_L, S, t_R, form, trunc_err, renormalize
 
 def _combine_constraints(good1, good2, warn):
     """return logical_and(good1, good2) if there remains at least one `True` entry.
