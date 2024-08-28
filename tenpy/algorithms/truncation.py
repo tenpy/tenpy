ce9969fbe0ae1293542a8ad1bff63c92fe0478bf
Johannes Hauschild <git@johannes-hauschild.de>
2024-08-28 15:57:07 +0200

    fixup: make flake8 ignore unsued import

--- tenpy/algorithms/truncation.py
+++ tenpy/algorithms/truncation.py
@@ -4,5 +4,9 @@ The `tenpy.algorithms.truncation` module was moved to :mod:`tenpy.linalg.truncat
 Here we just import all the names again for backwards compatibility,
 to support loading pickle and HDF5 data files with `TruncationError` instances in them.
 """
+# just provide namespace from ..linalg.truncation
 
-from ..linalg.truncation import *
+from ..linalg import truncation
+from ..linalg.truncation import TruncationError, truncate, svd_theta, decompose_theta_qr_based  # noqa: F401
+
+__all__ = truncation.__all__
