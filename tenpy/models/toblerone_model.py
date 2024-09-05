fc77aaeea045de53ff5f518f83da83b7048bf082
Gunnar Moller <G.Moller@kent.ac.uk>
2024-09-05 18:45:50 +0100

    updates to nomenclature of terms present for toblerone model

--- tenpy/models/toblerone_model.py
+++ tenpy/models/toblerone_model.py
@@ -22,8 +22,9 @@ class TobleroneModel(CouplingMPOModel):
             - h \sum_j \S_j^z
             
           = - \sum_x ( J1 S_(x,0)^z S_(x,1)^z + J1 S_(x,1)^z S_(x,2)^z + J2 S_(x,2)^z S_(x,0)^z )
-            - h \sum_(x,j) S_(x,j)^x
-            - \sum_(x,j) ( Jp S_(x,j)^z S_(x+1,j)^z )
+            - hx \sum_x (S_(x,0)^x + S_(x,2)^x)
+            - hxt \sum_(x) S_(x,1)^x
+            - \sum_x ( Jp (S_(x,0)^z S_(x+1,0)^z + S_(x,2)^z S_(x+1,2)^z ) +  Jpt S_(x,1)^z S_(x+1,1)^z)
             
     where the prior term is the nearest neighbour interaction and the latter is
     the on-site interaction with an external magnetic field.
@@ -38,10 +39,12 @@ class TobleroneModel(CouplingMPOModel):
     .. cfg.config :: TobleroneModel
         :include: CouplingMPOModel
         
-        J1, J2, Jp : float
-            Strength of the interactions between particles.
+        J1, J2, Jp, Jpt : float
+            Strength of the interactions between spins.
         hx : float
-            Strength of the externally applied magnetic field (z component).
+            Strength of the externally applied magnetic field for ladder spins (z component).
+        hxt : float
+            Strength of the externally applied magnetic field for top spins (z component).
         S : array
             The spin matrices, e.g. S^z = [[0,1],[1,0]].
         L : int
@@ -56,11 +59,15 @@ class TobleroneModel(CouplingMPOModel):
         J1 = model_params.get('J1', 1.) #"." forces float instead of int.
         J2 = model_params.get('J2', 2.)
         Jp = model_params.get('Jp', 1.)
+        Jpt = model_params.get('Jpt', 0.)
         hx = model_params.get('hx', 1.)
+        hxt = model_params.get('hxt', 1.)
         
         #External magnetic field interaction.
-        for j in range(3):
-            self.add_onsite(hx, j, 'Sx')
+
+        self.add_onsite(hx, 0, 'Sx')
+        self.add_onsite(hxt, 1, 'Sx')
+        self.add_onsite(hx, 2, 'Sx')
             
         #Coupling between lattice sites in the same unit cell.
         dx = 0 ##sort array
@@ -70,8 +77,10 @@ class TobleroneModel(CouplingMPOModel):
         
         #Coupling between lattice sites in adjacent unit cells.
         dx = 1
-        for j in range(3):
-            self.add_coupling(Jp, j, 'Sz', j, 'Sz', dx)
+        
+        self.add_coupling(Jp, 0, 'Sz', 0, 'Sz', dx)
+        self.add_coupling(Jpt, 1, 'Sz', 1, 'Sz', dx)
+        self.add_coupling(Jp, 2, 'Sz', 2, 'Sz', dx)
         
     def init_sites(self, model_params):
         spin = SpinHalfSite(conserve=None) #Defines the type of site (spin-half).
@@ -80,7 +89,7 @@ class TobleroneModel(CouplingMPOModel):
     def init_lattice(self, model_params):
         bc_MPS = model_params.get('bc_MPS', 'finite')
         sites = self.init_sites(model_params)
-        L = 1 #Length of the ladder
+        L = model_params.get('L', 1) #Length of the ladder
         lat = NLegLadder(L, 3, sites) #Define order and bc_MPS?:
         #lat = NLegLadder(L, 3, sites, order, bcMPS)
         return lat
