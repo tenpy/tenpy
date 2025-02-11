4175f4f50147dd2d84dc029eda7d8103f282dd09
bartandrews <bartandrews123@gmail.com>
2025-02-11 15:03:10 +0100

    remove dict type hint specifier

--- tenpy/models/molecular.py
+++ tenpy/models/molecular.py
@@ -3,7 +3,6 @@
 
 import itertools
 import numpy as np
-from typing import Any
 
 from .model import CouplingMPOModel
 from .lattice import Lattice
@@ -57,7 +56,7 @@ class MolecularModel(CouplingMPOModel):
             Constant.
     """
 
-    def __init__(self, params: dict[str, Any]):
+    def __init__(self, params: dict):
         if "one_body_tensor" in params and isinstance(params["one_body_tensor"], np.ndarray):
             self.one_body_tensor = params["one_body_tensor"]
         else:
