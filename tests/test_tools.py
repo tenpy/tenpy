9ee430b1e03ecee12ae43d59dd23cf8f820c60f1
Jakob Unfried <jakob.unfried@tum.de>
2025-02-14 16:19:16 +0000

    Add test that detects the bug of forum 757

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -305,7 +305,32 @@ def test_convert_memory_units():
 
 def test_setup_logging():
     tenpy.tools.misc.setup_logging(to_stdout="INFO", skip_setup=False)
-    
+
+
+def test_fixes_consistency_check_IrregularLattice():
+    # tests if the bug reported at https://tenpy.johannes-hauschild.de/viewtopic.php?t=757 is fixed.
+
+    class SpinModel_triangular_finite(tenpy.models.CouplingMPOModel):
+        def init_sites(self, model_params):
+            return tenpy.SpinHalfSite(conserve=None, sort_charge=True)
+
+        def init_terms(self, model_params):
+            self.add_onsite(-1, 0, 'Sz')
+
+        def init_lattice(self, model_params):
+            regular_lat = tenpy.Triangular(5, 5, self.init_sites(model_params))
+            return tenpy.IrregularLattice(regular_lat, remove=[(0, 0, 0)])
+
+    model = SpinModel_triangular_finite({})
+
+    ad_hoc_fix = False
+    if ad_hoc_fix:
+        model.lat.N_sites_per_ring = 1
+
+    psi = tenpy.MPS.from_lat_product_state(model.lat, [[['up']]])
+    dmrg_params = dict(max_sweeps=2)
+    _ = tenpy.algorithms.dmrg.run(psi, model, dmrg_params)
+
 
 if __name__ == "__main__":
     import tempfile
