3c3304e2a7a1b90a9cd983c095da6678b9e7d2b1
Jakob Unfried <jakob.unfried@tum.de>
2025-11-11 15:55:18 +0100

    VERSION 1.1.0

--- tenpy/version.py
+++ tenpy/version.py
@@ -20,10 +20,10 @@ __all__ = ['version', 'released', 'short_version', 'git_revision', 'full_version
 
 # hard-coded version for people without git...
 #: current release version as a string
-version = '1.0.7'
+version = '1.1.0'
 
 #: whether this is a released version or modified
-released = False
+released = True
 
 #: same as version, but with 'v' in front
 short_version = 'v' + version
