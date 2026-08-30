4300b02db14fedf0d9437571220acc07f3ecb685
Jakob Unfried <unfriedjakob@gmail.com>
2026-08-30 23:28:41 +0200

    Dummy version

--- tenpy/version.py
+++ tenpy/version.py
@@ -20,7 +20,7 @@ __all__ = ['version', 'released', 'short_version', 'git_revision', 'full_version
 
 # hard-coded version for people without git...
 #: current release version as a string
-version = '1.1.1'
+version = '1.1.99991'
 
 #: whether this is a released version or modified
 released = False
